a8bb6255f773a81db695299ae43b3e35d26a2371
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-17 15:25:51 -0300

    5th challenge, returning sum of points for a football team

--- src/challenges.js
+++ src/challenges.js
@@ -25,8 +25,8 @@ function concatName(nameList) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (3 * wins) + ties;
 }
 
 // Desafio 6
