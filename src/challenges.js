bb4c00b67c1582d5b52d2fa07d719c16283739f2
Marcelo Pessini <marceloppessini@gmail.com>
2020-07-17 15:06:10 -0300

    teste função &&2

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
-function compareTrue(a = true, b = false) {
-  if (a = true && b = true){
+function compareTrue(a, b) {
+  if ((a === true) && (b === true)){
     return true;
   } else {
     return false;
