ce4ecbfabefb5c13ba672981144a5dd0ebbbd1fa
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-07-17 18:45:19 -0300

    Ajuste do Requisito 07

--- src/challenges.js
+++ src/challenges.js
@@ -91,9 +91,9 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanciaCat1 = Math.abs(cat1 - mouse);
   let distanciaCat2 = Math.abs(cat2 - mouse);
   if (distanciaCat1 > distanciaCat2) {
-    return cat2;
+    return 'cat2';
   } else if (distanciaCat1 < distanciaCat2) {
-    return cat1;
+    return 'cat1';
   } else {
     return 'os gatos trombam e o rato foge';
   }
