f2ce0142e6b84eb763f450f8baba3ef3941c3a4e
Isadora Koga <isadorakoga710@gmail.com>
2020-07-17 18:39:14 -0300

    Ex 11 corrigido

--- src/challenges.js
+++ src/challenges.js
@@ -126,7 +126,7 @@ function techList(array, name) {
 function generatePhoneNumber(array) {
   // seu código aqui
   let numeroTelefone = '(' + array[0] + array[1] + ') ' + array[2] + array[3] + array [4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
-  if (array.length > 11) {
+  if (array.length !== 11) {
     return 'Array com tamanho incorreto';
   }
   for (let i = 0; i < array.length; i += 1) {
