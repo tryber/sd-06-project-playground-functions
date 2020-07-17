07e9ec5a132e2525b0b5e7574e2238dcc93efb13
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-07-17 16:09:46 -0300

    Challenge 5 - Done

--- src/challenges.js
+++ src/challenges.js
@@ -28,9 +28,11 @@ function concatName(lista) {
 //console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (wins * 3) + (ties);
+  return points;
 }
+//console.log(footballPoints(3, 1));
 
 // Desafio 6
 function highestCount() {
