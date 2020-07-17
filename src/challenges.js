a92773b857ca0f0a125fed2c14d3e1961efde18e
Dilenio Enderle <dilenio@gmail.com>
2020-07-17 14:55:29 -0300

    2 - Área do triângulo

--- src/challenges.js
+++ src/challenges.js
@@ -1,16 +1,17 @@
 // Desafio 1
 function compareTrue(value1, value2) {
   // seu código aqui
-  if ( value1 === true && value2 === true) {
+  if (value1 === true && value2 === true) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
