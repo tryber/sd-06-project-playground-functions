44750c7014fdf1fe6d4b1638ec131b76ba877d7a
Daniele Perse <danieleperse3@gmail.com>
2020-07-17 15:47:02 -0300

    corrigi desafio 1 e terminei desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,15 +1,11 @@
 // Desafio 1
 function compareTrue(a, b) {
-  if (a && b > 10) {
-    return true;
-  } else {
-    return false;
-  }
+  return a && b;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * altura) / 2;
 }
 
 // Desafio 3
