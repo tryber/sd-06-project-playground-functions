f4146aeaaf32bf98f669e96238325791d265eb3e
rafMewtwo <ponciano.raf@gmail.com>
2020-07-17 18:59:00 -0300

    result 09

--- src/challenges.js
+++ src/challenges.js
@@ -136,11 +136,24 @@ function fizzBuzz(array) {
 console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
-function encode() {
+function encode(string) {
   // seu código aqui
+  let result = string.replace(/a/g,1);
+  result = result.replace(/e/g,2);
+  result = result.replace(/i/g,3);
+  result = result.replace(/o/g,4);
+  result = result.replace(/u/g,5);
+  return result;
 }
-function decode() {
+
+function decode(string) {
   // seu código aqui
+  let result = string.replace(/1/g,a);
+  result = result.replace(/2/g,e);
+  result = result.replace(/3/g,i);
+  result = result.replace(/4/g,o;
+  result = result.replace(/5/g,u);
+  return result;
 }
 
 // Desafio 10
