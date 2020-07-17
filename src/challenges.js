284f556e43c2c0287983171836f3dd64880fa905
Debora Polesel <deborahpolesel@hotmail.com>
2020-07-17 16:21:22 -0300

    Exercise 3

--- src/challenges.js
+++ src/challenges.js
@@ -12,8 +12,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase) {
+  let newArray = phrase.split(" ");
+  return newArray;
 }
 
 // Desafio 4
