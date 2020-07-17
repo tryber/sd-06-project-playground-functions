207c6284177f509cefde32d65e792057f591cd34
Leandro Parisi <lepcbelisario@gmail.com>
2020-07-17 15:06:41 -0300

    Requisito 2 - function completa + chamando ela apos

--- src/challenges.js
+++ src/challenges.js
@@ -10,9 +10,11 @@ function compareTrue (param1, param2) {
 compareTrue (true, false);
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base / height) / 2;
 }
+
+calcArea (20, 2);
 
 // Desafio 3
 function splitSentence() {
