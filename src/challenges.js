8a5370533f52174c2836b2db98bbea568bd2ce88
CristianoNunes <cristianonunes99@gmail.com>
2020-07-17 17:33:05 -0300

    Requisito 5 Concluido

--- src/challenges.js
+++ src/challenges.js
@@ -28,8 +28,10 @@ function concatName(arr) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let winsPoints = wins * 3;
+  let points = winsPoints + ties;
+  return points;
 }
 
 // Desafio 6
