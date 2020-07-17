5b454a5ef51c8e3c062a67f725607dd51eee05c3
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-07-17 15:31:08 -0300

    Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -9,8 +9,10 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  string = string.split(' ');
+
+  return string;
 }
 
 // Desafio 4
