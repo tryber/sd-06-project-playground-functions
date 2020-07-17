86fee982ed6e5c9db43182af4c3018020d0eff5e
Alvaro Neto <alvaroshn@gmail.com>
2020-07-17 18:45:43 -0300

    desafio do bar, versao 4

--- src/challenges.js
+++ src/challenges.js
@@ -168,7 +168,7 @@ function generatePhoneNumber(numeroTelefone) {
   return numeroFormatado;
   
 }
- generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
+ //generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
 // Desafio 12
 function triangleCheck(lineA, lineB , lineC) {
   if (((lineA < lineB + lineC)&&(lineA > Math.abs(lineB - lineC)))||((lineB < lineA + lineC)&&(lineB > Math.abs(lineA - lineC)))||((lineC < lineB + lineC)&&(lineC > Math.abs(lineB - lineA)))){
