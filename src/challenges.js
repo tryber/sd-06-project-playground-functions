59058b2e00c8c29d6ce0630f94ffd632279f1198
Lucas Miranda <lucasmirandadev@gmail.com>
2020-07-17 19:11:28 -0300

    exercicio 05

--- src/challenges.js
+++ src/challenges.js
@@ -34,8 +34,9 @@ function concatName() {
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
