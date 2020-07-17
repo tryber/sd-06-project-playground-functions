89a2af82eb22fbe0455cf2f3a401e3f04ccbbbeb
Rodrigo Derkian <derkianpromo@gmail.com>
2020-07-17 19:18:21 -0300

    Adicionando 3ª função

--- src/challenges.js
+++ src/challenges.js
@@ -7,12 +7,12 @@ return false;
 }
 // Desafio 2
 function calcArea(base, height) {
-return base * height/2
+return base * height/2;=
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
+return splitSentence.split(" ", 2)
 }
 
 // Desafio 4
