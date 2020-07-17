91b16ef060f223755ccaa7e09e9f3df4921a93f3
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-07-17 18:02:22 -0300

    exercicios obrigatorios

--- src/challenges.js
+++ src/challenges.js
@@ -1,71 +1,237 @@
 // Desafio 1 // Usando operador &&
-function compareTrue() {
-  // seu código aqui
+
+let param1=true;
+let param2=false;
+
+function compareTrue(param1,param2) {
+  if (param1 && param2){
+    return true
+  }
+  else {
+    return false
+  }
+}
+console.log(compareTrue(param1,param2))
+
+
+
+// Desafio 2 // Área do triângulo
+
+function calcArea(b,h) {
+  area=(b*h)/2
+  return area
 }
 
-// Desafio 2
-function calcArea() {
-  // seu código aqui
+let b=10;
+let h=15;
+let area;
+console.log(calcArea(b,h))
+
+
+
+// Desafio 3 // Dividindo a frase
+
+function splitSentence(sentence) {
+  return sentence.split(" ");
 }
 
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
+let sentence="go Trybe";
+console.log(splitSentence(sentence))
+
+
+
+// Desafio 4 // Concatenação de strings
+
+function concatName(arrayStrings) {
+  return arrayStrings[arrayStrings.length-1] + ", "  + arrayStrings[0];
 }
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
+let arrayStrings=['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+console.log(concatName(arrayStrings))
+
+
+
+// Desafio 5 // Pontos no futebol
+
+
+function footballPoints(gamesWon,gamesTied) {
+  points=(gamesTied*1)+(gamesWon*3)
+  return points
 }
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
+let gamesWon=2;
+let gamesTied=3;
+let points;
+console.log(footballPoints(gamesWon,gamesTied))
+
+
+
+// Desafio 6 // Repetição do maior número
+
+function highestCount(numbers) {
+
+  let maxQtd=0;
+  let objeto={};
+  let numeroRepetido;
+
+  for (let i in numbers){
+
+    let n = numbers[i];
+
+    if (objeto[n]){
+      objeto[n]+=1
+
+      if (objeto[n]>maxQtd){
+        maxQtd=objeto[n];
+        numeroRepetido=i;
+      }
+    }
+    else {
+      objeto[n]=1
+    }
+  }
+  return maxQtd;
+}
+let numbers=[9, 1, 2, 3, 9, 5, 7];
+console.log(highestCount(numbers))
+
+
+
+// Desafio 7 // Caça ao rato
+
+function catAndMouse(param1,param2,param3) {
+
+  let dist1=gat1-rat;
+  let dist2=gat2-rat;
+
+  if (Math.abs(dist1>dist2)){
+    return "gato1 posicao:" + gat1
+  }
+  else if(Math.abs(dist1<dist2)){
+    return "gato2 posicao:" + gat2
+  }
+  else if (Math.abs(dist1=dist2)){
+    return "os gatos trombam e o rato foge"
+  }
 }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
+let rat=10;
+let gat1=3;
+let gat2=2;
+console.log(catAndMouse(rat,gat1,gat2))
+
+
+
+// Desafio 8 // FizzBuzz
+
+function fizzBuzz(arr) {
+
+  let resultado=[]
+
+  for (let i in arr){
+
+    if (arr[i]%3==0 && arr[i]%5==0){
+      resultado.push("fizzBuzz") 
+    }
+
+    else if (arr[i]%3==0){
+      resultado.push("fizz")
+    }
+
+    else if (arr[i]%5==0){
+      resultado.push("buzz")
+    }
+
+    else {
+      resultado.push("bug!")
+    }
+  }
+  return resultado;
 }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
+let numbers2=[2, 15, 7, 9, 45];
+console.log(fizzBuzz(numbers2))
+
+
+
+// Desafio 9 // Codifique e Decodifique
+
+function encode(frase) {
+  codificada=frase.replace(/a/g,"1").replace(/e/g,"2").replace(/i/g,"3").replace(/o/g,"4").replace(/u/g,"5");
+  return codificada
 }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function decode(decodificada) {
+  decodificada=codificada.replace(/1/g,"a").replace(/2/g,"e").replace(/3/g,"i").replace(/4/g,"o").replace(/5/g,"u");
+  return decodificada
 }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
+let frase="hi there!";
+let codificada;
+let decodificada;
+console.log(encode(frase))
+console.log(decode(decodificada))
+
+
+
+// Desafio 10 // Lista de tecnologias
+
+function techList(nomesTec, nome) {
+  
+  let saida=[];
+  let obj={};
+
+  for (let i in nomesTec){
+    obj={
+      tech:nomesTec[i],
+      name:nome
+    }
+    saida.push(obj);
+  }
+
+    return saida.sort(function(a, b){
+      if(a.tech<b.tech){
+        return -1;
+      }
+      else if (a.tech>b.tech){
+        return +1;
+      }
+      else {
+        return 0;
+      }
+    }
+  )
 }
 
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
+let nomesTec=["React", "Jest", "HTML", "CSS", "JavaScript"];
+let nome="Lucas";
+console.log(techList(nomesTec, nome))
 
-// Desafio 11
+
+
+
+// Desafio 11 // Número de telefone
+
 function generatePhoneNumber() {
   // seu código aqui
 }
 
-// Desafio 12
+// Desafio 12 // Condição de existência de um triângulo
+
 function triangleCheck() {
   // seu código aqui
 }
 
-// Desafio 13
+// Desafio 13 // Bem vindo ao Bar da Trybe!
+
 function hydrate() {
   // seu código aqui
 }
 
+
+
+
+// NAO MEXER A PARTIR DAQUI
 
 module.exports = {
   calcArea,
