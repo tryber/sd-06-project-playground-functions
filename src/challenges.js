b96c5cd88d800f88dff425886516339ca308d7b3
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-07-17 19:40:05 -0300

    Challenge 11 - Done

--- src/challenges.js
+++ src/challenges.js
@@ -184,9 +184,14 @@ function timesCount(list) {
 // console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(a, b, c) {
+  if (((a < (b + c)) && (a > Math.abs(b - c))) && ((b < (a + c)) && (b > Math.abs(a - c))) && ((c < (b + a)) && (c > Math.abs(b - a)))) {
+    return true;
+  } else {
+    return false;
+  }
 }
+console.log(triangleCheck(10, 14, 8));
 
 // Desafio 13
 function hydrate() {
