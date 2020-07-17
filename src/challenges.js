ca99d1e6744eadb4363af7d0695b8a9ef4e247a1
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-07-17 19:04:05 -0300

    correcao exercicios

--- src/challenges.js
+++ src/challenges.js
@@ -158,20 +158,20 @@ console.log(fizzBuzz(numbers2))
 // Desafio 9 // Codifique e Decodifique
 
 function encode(frase) {
-  codificada=frase.replace(/a/g,"1").replace(/e/g,"2").replace(/i/g,"3").replace(/o/g,"4").replace(/u/g,"5");
+  let codificada=frase.replace(/a/g,"1").replace(/e/g,"2").replace(/i/g,"3").replace(/o/g,"4").replace(/u/g,"5");
   return codificada
 }
 
 function decode(codificada) {
-  decodificada=codificada.replace(/1/g,"a").replace(/2/g,"e").replace(/3/g,"i").replace(/4/g,"o").replace(/5/g,"u");
+  let decodificada=codificada.replace(/1/g,"a").replace(/2/g,"e").replace(/3/g,"i").replace(/4/g,"o").replace(/5/g,"u");
   return decodificada
 }
 
 let frase="hi there!";
-let codificada;
-let decodificada;
-console.log(encode(frase))
-console.log(decode(decodificada))
+let codificada=encode(frase);
+
+console.log(codificada)
+console.log(decode(codificada))
 
 
 
