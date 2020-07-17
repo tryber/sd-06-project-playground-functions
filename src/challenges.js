37c213457e7ef6802f10a525517db1efbaef7a9e
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-17 16:43:43 -0300

    Desafio 4: Concatenando strings

--- src/challenges.js
+++ src/challenges.js
@@ -5,8 +5,8 @@
 function compareTrue(valor1, valor2) {
   if ((valor1 === true) && (valor2 === true)) {
     return true;
-  } 
-  return false;  
+  }
+  return false;
 }
 
 // Desafio 2
@@ -16,23 +16,26 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(str) {
-  let nome = "";
+  let nome = '';
   let arrayNome = [];
-  for (index = 0; index < str.length; index += 1) {
-    if (str[index] !== " ") {
+  for (let index = 0; index < str.length; index += 1) {
+    if (str[index] !== ' ') {
       nome += str[index];
-  } else {
-    arrayNome.push(nome);
-    nome = "";
-  }
+    } else {
+      arrayNome.push(nome);
+      nome = '';
+    }
   }
   arrayNome.push(nome);
   return arrayNome;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(trocaNome) {
+  let primeiroNome = trocaNome[trocaNome.length - 1];
+  let ultimoNome = trocaNome[0];
+  let stringFinal = primeiroNome + ", " + ultimoNome;
+  return stringFinal;  
 }
 
 // Desafio 5
