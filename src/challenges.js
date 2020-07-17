dc5f6d2d44c4480cc89e4743158c05db7dd41956
Cyrano <cyrano@cyranowebdev.com>
2020-07-17 15:33:20 -0300

    adicionando exercicio 5 a challenges.js

--- src/challenges.js
+++ src/challenges.js
@@ -1,49 +1,55 @@
 // Desafio 1
-let teste = false;
 function compareTrue(boo1, boo2) {
   // seu código aqui
-  if (boo1 === true && boo2 === true) {
-    teste = true;
+  let teste = false;
+  if (typeof boo1 === 'boolean' && typeof boo2 === 'boolean') {
+    if (boo1 === true && boo2 === true) {
+      teste = true;
+      return teste;
+    }
+    else {
+      teste = false;
+      return teste;
+    }
   }
   else {
-    teste = false;
+    return 'erro: parâmetros devem ser true ou false.';
   }
-  return teste;
 }
 console.log(compareTrue(true, false));
 
 // Desafio 2
-let areaDoTriangulo = 0;
 function calcArea(base, alt) {
   // seu código aqui
+  let areaDoTriangulo = 0;
   if (typeof base === 'number' && typeof alt === 'number' && base > 0 && alt > 0) {
     areaDoTriangulo = (base * alt) / 2;
+    return areaDoTriangulo;
   }
   else {
-    console.log('erro: insira dois números positivos.');
+    return 'erro: insira dois números positivos.';
   }
-  return areaDoTriangulo;
 }
 console.log(calcArea(12,8));
 
 // Desafio 3
-let sentenceArray = [];
 function splitSentence(string) {
   // seu código aqui
+  let sentenceArray = [];
   if (typeof string === 'string') {
     sentenceArray = string.split(" ");
+    return sentenceArray;
   }
   else {
-    console.log('erro: parâmetro deve ser um texto.');
+    return 'erro: parâmetro deve ser um texto.';
   }
-  return sentenceArray;
 }
 console.log(splitSentence('go Trybe !'));
 
 // Desafio 4
-let arrayStrings = [];
 function concatName(array) {
   // seu código aqui
+  let arrayStrings = [];
   arrayStrings.push(array[array.length - 1]);
   arrayStrings.push(array[0]);
   return arrayStrings;
@@ -51,9 +57,19 @@ function concatName(array) {
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(win, ties) {
   // seu código aqui
+  let points = 0;
+  if (Number.isInteger(win) && Number.isInteger(ties) && win >= 0 && ties >= 0) {
+    points += (win * 3);
+    points += (ties); 
+    return points; 
+  }
+  else {
+    return 'only positive integers allowed.';
+  }
 }
+console.log(footballPoints(1,2));
 
 // Desafio 6
 function highestCount() {
