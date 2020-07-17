63bedb59f2fe5379f1e16e8bbfc5dd62f0d99180
Vagner Pereira <sousa.vagner82@gmail.com>
2020-07-17 18:05:57 -0300

    Add exercicio 7

--- src/challenges.js
+++ src/challenges.js
@@ -48,10 +48,10 @@ function highestCount(numeros) {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   if(mouse - cat1 > mouse - cat2){
-    return cat2;
+    return "cat2";
   }else
   if(mouse - cat1 < mouse - cat2){
-    return cat1;
+    return "cat1";
   }else{
     return "os gatos trombam e o rato foge";
   }
