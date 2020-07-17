8ecc233577a6d6cac6023c8643ed448d0f42ec18
ricknois <rickssti@outlook.com>
2020-07-17 15:23:14 -0300

    Primeiro commit, Exercicio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(val1, val2) {
+  if (val1 === true && val2 === true) {
+    return true;
+  } else {
+    return false;
+  } 
 }
 
 // Desafio 2
