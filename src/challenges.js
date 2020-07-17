12951494291151fd439f3d48f9e6b5ab8ae1215b
Marthus Sera <marthus2001@gmail.com>
2020-07-17 20:31:17 -0300

    Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -166,11 +166,43 @@ let d8 = [2, 15, 7, 9, 45]
 console.log(fizzBuzz(d8))
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(str) {
+  
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == "a") {
+      str[i] = "1"
+    } else if (str[i] == "e") {
+      str[i] = "2"
+    } else if (str[i] == "i") {
+      str[i] = "3"
+    } else if (str[i] == "o") {
+      str[i] = "4"
+    } else if (str[i] == "u") {
+      str[i] = "5"
+    }
+  }
+
+  return str
+
 }
-function decode() {
-  // seu código aqui
+function decode(str) {
+  
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == "1") {
+      str[i] = "a"
+    } else if (str[i] == "2") {
+      str[i] = "e"
+    } else if (str[i] == "3") {
+      str[i] = "i"
+    } else if (str[i] == "4") {
+      str[i] = "o"
+    } else if (str[i] == "5") {
+      str[i] = "u"
+    }
+  }
+
+  return str
+
 }
 
 // Desafio 10
