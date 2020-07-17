a6e97fe0ace806bfd41e7b7e23047112e3303bac
Ricardo <ricardorosa@gmail.com>
2020-07-17 14:57:32 -0300

    Exercício 1 - Comparar se dois valores são verdadeiros, ou se um deles é falso, utilizando o &&.

--- src/challenges.js
+++ src/challenges.js
@@ -1,86 +1,93 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(param1, param2) {
+    // seu código aqui
+    if (param1 == true && param2 == true) {
+        console.log("Ambos os valores são verdadeiros!");
+        return true;
+    } else {
+        console.log("Algum dos valores é falso!");
+        return false;
+    }
 }
 
 // Desafio 2
 function calcArea() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 4
 function concatName() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 5
 function footballPoints() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 7
 function catAndMouse() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 9
 function encode() {
-  // seu código aqui
+    // seu código aqui
 }
+
 function decode() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 10
 function techList() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 12
 function triangleCheck() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 13
 function hydrate() {
-  // seu código aqui
+    // seu código aqui
 }
 
 
 module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  generatePhoneNumber,
-  techList,
-  highestCount,
-  hydrate,
-  splitSentence,
-  triangleCheck,
+    calcArea,
+    catAndMouse,
+    compareTrue,
+    concatName,
+    decode,
+    encode,
+    fizzBuzz,
+    footballPoints,
+    generatePhoneNumber,
+    techList,
+    highestCount,
+    hydrate,
+    splitSentence,
+    triangleCheck,
 }
