49873111ee6b4d87e6832e6ee15c6bc0b69c79a5
vapatty6848 <vapatty@gmail.com>
2020-07-17 15:09:47 -0300

    executando a funçao compareTrue

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,22 @@
 // Desafio 1 - testando um commit
-function compareTrue() {
+/* //Usando o operador &&
+JavaScript possui um operador lógico &&, o qual recebe dois valores e retorna true se ambos os valores são verdadeiros, e retorna false
+se algum dos valores não o for.
+Considerando isso, crie uma função chamada compareTrue que, ao receber dois booleanos:
+Retorne true se ambos os valores são verdadeiros;
+Retorne false se um ou ambos os parâmetros forem falsos.
+Faça a função utilizando o operador &&. */
+function compareTrue( a, b) {
   // seu código aqui
+  if (a === true &&  b === true) {
+     return ;
+  } else {
+     return ;
+  }
 }
+let a = false;
+let b = true;
+compareTrue(a, b);
 
 // Desafio 2
 function calcArea() {
