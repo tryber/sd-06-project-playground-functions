dbcf7a983e51b068a77f9876f695ed24daa57150
Guilherme Persch <persch.gui@gmail.com>
2020-07-17 15:04:13 -0300

    desafio_1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1 iniciando
-function compareTrue() {
-  // seu código aqui
+function compareTrue(parametro1, parametro2) {
+  if (parametro1 && parametro2) {
+    return true;
+  }else {
+    return false;
+  }
 }
 
 // Desafio 2
