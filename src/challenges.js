1142a2b66c53ba64ce9e48f70d7a678f074a3379
Marxneves <marxkhristie@gmail.com>
2020-07-17 15:21:07 -0300

    Desafio 3 e 4 finalizado

--- src/challenges.js
+++ src/challenges.js
@@ -13,13 +13,13 @@ function calcArea(base, height) {
 
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ');
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return (`${array[array.length-1]}, ${array[0]}.`);
 }
 
 // Desafio 5
