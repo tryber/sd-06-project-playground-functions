b9ada4c4a142dfd6a9d017926e70c6a48c237439
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-17 15:34:59 -0300

    Adding exercise 4 (done)

--- src/challenges.js
+++ src/challenges.js
@@ -24,9 +24,12 @@ function splitSentence() {
 }
 
 // Desafio 4
-function concatName() {
+function concatName(stringArray) {
   // seu código aqui
+  return (stringArray[stringArray.length - 1] + ", " +  stringArray[0])
 }
+
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
 
 // Desafio 5
 function footballPoints() {
