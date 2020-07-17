97721f7a0f7cf3d78543b04a7b0ab876d7104307
Alana Monteiro <lanaah.15@gmail.com>
2020-07-17 20:23:40 -0300

    footballPoints

--- src/challenges.js
+++ src/challenges.js
@@ -32,8 +32,8 @@ function concatName(names) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins,ties){
+  return wins*3 + ties;
 }
 
 // Desafio 6
@@ -52,8 +52,6 @@ function highestCount(numbers) {
   }
   return counter;
 }
-
-
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let distCat1 = cat1 - mouse;
