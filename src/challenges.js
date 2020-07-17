3570f3a4228e349ac325ad564853d8ef9a0826b0
Danilo <dansantosalfa@gmail.com>
2020-07-17 15:07:03 -0300

    Segunda Questão

--- src/challenges.js
+++ src/challenges.js
@@ -9,9 +9,11 @@ function compareTrue(q1x, q1y) {
   }
 }
 
+let base = 2;
+let height = 4;
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base*height)/2;
 }
 
 // Desafio 3
