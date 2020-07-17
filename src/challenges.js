85353d71c31e33ab07269bb02f89d3ed85989a5d
Anderson <and.stuber@gmail.com>
2020-07-17 14:48:00 -0300

    Exercicio 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,73 +1,61 @@
 // Desafio 1
 function compareTrue(a = 1, b = 1) {
-  // seu código aqui
   if (a && b == 1){
     return true
   }
   else 
     return false
 }
+
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base = 10, height = 8) {
+  return (base * height) / 2
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
 }
 
 // Desafio 4
 function concatName() {
-  // seu código aqui
 }
 
 // Desafio 5
 function footballPoints() {
-  // seu código aqui
 }
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
 }
 
 // Desafio 7
 function catAndMouse() {
-  // seu código aqui
 }
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
 }
 
 // Desafio 9
 function encode() {
-  // seu código aqui
 }
 function decode() {
-  // seu código aqui
 }
 
 // Desafio 10
 function techList() {
-  // seu código aqui
 }
 
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
 }
 
 // Desafio 12
 function triangleCheck() {
-  // seu código aqui
 }
 
 // Desafio 13
 function hydrate() {
-  // seu código aqui
 }
 
 
