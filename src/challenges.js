4aa2c6016e176436e65bd63812bfad0f07c067ae
Stefano Branz <stefano.branz@gmail.com>
2020-07-17 16:18:42 -0300

    Testing second exercise

--- src/challenges.js
+++ src/challenges.js
@@ -9,8 +9,8 @@ function compareTrue(value1, value2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
 }
 
 // Desafio 3
