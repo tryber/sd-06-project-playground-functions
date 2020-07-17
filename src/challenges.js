2b5d64848c618fa0a3bfb104b404081678501084
us19861229c <rcks.santos@gmail.com>
2020-07-17 16:08:55 -0300

    Desafio 4. Edita a função concatNames()

--- src/challenges.js
+++ src/challenges.js
@@ -17,8 +17,8 @@ function splitSentence(frase) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(itens) {
+  return [itens[itens.length - 1],itens[0]]
 }
 
 // Desafio 5
