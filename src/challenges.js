6607ba59ffc0bb39ac9c6f51618bf7f08fe3c4fc
Jean Machado <jeanvictor.arq@gmail.com>
2020-07-17 18:32:25 -0300

    exerc-5-tent-1

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,8 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if (value1 == true && value2 == true) {
+  if (value1 === true && value2 === true) {
     return true;
-  } else if (value1 == false || value2 == false) {
+  } else if (value1 === false || value2 === false) {
     return false;
   }
 } console.log(compareTrue(false, false));
@@ -11,43 +11,42 @@ function compareTrue(value1, value2) {
 function calcArea(base, height) {
   let calc = (base * height) / 2;
   return calc;
-} console.log(calcArea(10, 10));
+} 
+console.log(calcArea(10, 10));
  
 // Desafio 3
 function splitSentence(string) {
   return string.split(" ");  
-} console.log(splitSentence("Hello World"));
+} 
+console.log(splitSentence("Hello World"));
 
 // Desafio 4
-
-//let array = ["Ana", "Jean", "Pedro", "Maria", "Joao"];
-
 function concatName(recebeArray) {
   return recebeArray[recebeArray.length -1] + ", " + recebeArray[0];
 } 
 console.log(concatName(nomes = ["Ana", "Jean", "Pedro", "Maria", "Joao"]));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let totalPontos = (wins * 3) + (ties * 1);
+  return totalPontos;
 }
+console.log(footballPoints(10, 5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Desafio 6
 function highestCount() {
