b8c2fd440bb839f3cb8b3976bd48f9de590a42c5
wfelipefarina <wfelipefarina@gmail.com>
2020-07-17 19:37:10 -0300

    Resolução exercicio 7

--- src/challenges.js
+++ src/challenges.js
@@ -50,6 +50,13 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanceCat1 = mouse - cat1;
   let distanceCat2 = mouse - cat2;
 
+  if (Math.sign(distanceCat1) < 0) {
+      distanceCat1 *= -1;
+  } else if (Math.sign(distanceCat2) < 0) {
+      distanceCat2 *= -1
+
+  }
+
   if (distanceCat1 < distanceCat2) {
       return "cat1";
   } else if (distanceCat2 < distanceCat1) {
@@ -59,7 +66,7 @@ function catAndMouse(mouse, cat1, cat2) {
 
 }
 
-console.log(catAndMouse(8, 3, 2));
+console.log(catAndMouse(0, 3, 2));
 
 // Desafio 8
 function fizzBuzz(numbers) {
