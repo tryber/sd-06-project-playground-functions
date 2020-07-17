c1bbcd6ea1625daae7a4e52021682e7c3663769d
Pedro Duarte <ikbenpedro@gmail.com>
2020-07-17 18:30:39 -0300

    desafio 4.1

--- src/challenges.js
+++ src/challenges.js
@@ -1,23 +1,30 @@
 // Desafio 1
 function compareTrue(value1, value2) {
+    //booleano simples
     return value1 && value2;
 }
 console.log(compareTrue(2, 2))
     // Desafio 2
 function calcArea(base, altura) {
+    //fazer a conta
     let result = ((base * altura) / 2);
     return result;
 }
 console.log(calcArea(3, 2))
     // Desafio 3
 function splitSentence(sentence) {
+    //usar function split
     divisao = sentence.split(' ');
     return divisao;
 }
 console.log(splitSentence("hello world"))
     // Desafio 4
 function concatName(nomesConcatenados) {
-    return [nomesConcatenados[nomesConcatenados.length - 1], nomesConcatenados[0]]
+    // criar const com o ultimo 
+    let palavra1 = nomesConcatenados[nomesConcatenados.length - 1]
+        //criar const para receber o elemento do array e concatenar com o primeiro elemento do array
+    let resultado = palavra1 + ", " + nomesConcatenados[0]
+    return resultado
 }
 let array = ["asda", "qweqweq", "zxczxc", "poipo"]
 console.log(concatName(array))
