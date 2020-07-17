6cf677d435824bd63b7c2b1e37e2c6ae6017a8e2
dekoPossas <andrepossaster@gmail.com>
2020-07-17 15:50:38 -0300

    segundo exercicio feito

--- src/challenges.js
+++ src/challenges.js
@@ -1,16 +1,14 @@
 // Desafio 1
 function compareTrue(a, b) {
-  // seu código aqui
   if (a === true && b === true){
     return true
   }else{
     return false
   }
 }
-compareTrue(true, false)
 // Desafio 2
 function calcArea() {
-  // seu código aqui
+  return (base * height)/2
 }
 
 // Desafio 3
