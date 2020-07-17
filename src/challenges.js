ee97de9b4a312b818c8ebe720b91f3ee82546d45
wfelipefarina <wfelipefarina@gmail.com>
2020-07-17 16:47:36 -0300

    Resolução exercicio 8

--- src/challenges.js
+++ src/challenges.js
@@ -51,9 +51,22 @@ function catAndMouse() {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+  for(let i = 0; i < numbers.length; i++) {
+      if(numbers[i] % 3 === 0 & numbers[i] % 5 === 0) {
+          console.log("fizzbuzz")
+      } else if (numbers[i] % 3 === 0) {
+          console.log ("fizz")
+      } else if (numbers[i] % 5 === 0) {
+          console.log("buzz")
+      } else {
+          console.log("bug")
+      }
+  }
 }
+
+
+fizzBuzz([2, 15, 7, 9, 45])
 
 // Desafio 9
 function encode() {
