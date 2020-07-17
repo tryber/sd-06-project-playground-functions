9afed34de6c53a71706c9515445a2709752e735c
ilanribeiro <ilanribeiro@gmail.com>
2020-07-17 16:03:27 -0300

    first commit to test the evaluator on the start of my code

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código
+function compareTrue(termo1, termo2) {
+  if (termo1 === termo2){
+    return "true";
+  } else {
+    return "false";
+  }
 }
 
 // Desafio 2
