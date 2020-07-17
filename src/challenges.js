bfa35b37c0e116fa9d3b03141956a5d891fccf5b
CleberFontinele <cleberfontinele2387@gmail.com>
2020-07-17 15:04:34 -0300

    Usando o operador &&

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(a, b) {
+  if (a === b) {
+    return true
+  } else {
+    return false
+  }
 }
+console.log(compareTrue(10, 100))
 
 // Desafio 2
 function calcArea() {
