8d194c9845c7f5cdd0479631e8f857c4cc7e5f6e
kissyla <kissylamottapereira@gmail.com>
2020-07-17 17:19:20 -0300

    tentativa: fizzbuzz

--- src/challenges.js
+++ src/challenges.js
@@ -15,8 +15,10 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  
+function splitSentence(string) {
+  let arrayString =[];
+  arrayString = string.split(" ");
+return arrayString;
 }
 
 // Desafio 4
@@ -25,28 +27,62 @@ function concatName(name) {
   for(let index = name.length -1 ; index >= 0 ; index -= 1){
    name2 += name[index];
   }
-  rseturn name2;
+  return name2;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = 0;
+  points = (wins * 3) + ties;
+  return points;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arra) {
+let higherNumber = arra[0];
+let qtdNumber = 0;
+let bigger=0;
+
+  for(let index = 0; index <= arra.length -1; index +=1 ){
+   if(arra[index]>higherNumber){
+     bigger = arra[index];
+   } 
+   if(higherNumber == arra[index]){
+     qtdNumber += 1;
+   }
+  }
+  return qtdNumber;
+  
 }
 
+
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  
+  for(let index =0 ; index <= array.length-1; index += 1){
+    if(array[index] % 3 == 0 && array[index] % 5 == 0){
+      return "fizzBuzz";
+    }else if(array[index] % 3 == 0 && array[index] % 5 != 0){
+      return "fizz";
+    }else if(array[index] % 3 != 0 && array[index] % 5 == 0){
+     return "buzz";
+    } else{
+      return "bug";
+    }
+  }
 }
+
+
+//cada número da Array que seja divisível por 3, apresente uma string "fizz";
+//Para cada número da Array que seja divisível por 5, apresente uma string "buzz";
+//Caso o número seja divisível por 3 ou 5, retorne a string "fizzBuzz";
+//Caso o número não possa ser dividido por 3 nem por 5, retorne a string "bug!";
+//Exemplo: caso o parâmetro seja [2, 15, 7, 9, 45], sua função deverá retornar ["bug!", "fizzBuzz", "bug!", "fizz", "fizzBuzz"].
 
 // Desafio 9
 function encode() {
