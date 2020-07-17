57189bae96a74946ca20d93aaceee01d4c588c4a
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-07-17 20:35:39 -0300

    11° execicio bonus

--- src/challenges.js
+++ src/challenges.js
@@ -183,7 +183,7 @@ function techList(arrayTecnologias, name) {
     for (let indiceTec in ordenarArray){
       saidaTec.push({
         tech: ordenarArray[indiceTec],
-        nome: name
+        nome: name,
       });
     }
     return saidaTec;
@@ -195,9 +195,32 @@ console.log(techList(valor1, valor2)); */
 
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(arrayTelefone) {
   // seu código aqui
+  if (arrayTelefone.length !== 11){
+    return "Array com tamanho incorreto."    
+  }else{
+    let escrevendoNumero = "";     
+    for (let numero in arrayTelefone){
+      if (arrayTelefone[numero] < 0 || arrayTelefone[numero] > 9){        
+        return "não é possível gerar um número de telefone com esses valores"      
+        break;
+      }else if (numero === "0"){
+        escrevendoNumero += "(" + arrayTelefone[numero];
+      }else if (numero === "1"){
+        escrevendoNumero += arrayTelefone[numero] + ") ";
+      }else if (numero === "6"){
+        escrevendoNumero += arrayTelefone[numero] + "-";
+      }else{
+        escrevendoNumero += arrayTelefone[numero];
+      }                     
+    } 
+    return escrevendoNumero;        
+  }
 }
+let valor1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+/* let valor2 = "Clayton Miguel"; */
+console.log(generatePhoneNumber(valor1));
 
 // Desafio 12
 function triangleCheck() {
