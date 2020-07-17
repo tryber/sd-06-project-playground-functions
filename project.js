de293269dff0c765b1c6f8accc13519fe93cce06
Luiz Phellipe <luizinhophe@hotmail.com>
2020-07-17 15:12:14 -0300

    Exercicio 1, 2 e 3 feitos

--- project.js
+++ project.js
@@ -1,1 +1,32 @@
+//Exercicio 1
+function compareTrue(bol1, bol2){
+    if(bol1 == true && bol2 == true){
+        return true;
+    }else{
 
+        return false;
+    }
+}
+console.log(compareTrue(true, true));
+
+
+//Exercício 2
+function calcArea(base, height){
+    let area = (base*height) / 2;
+    return area; 
+}
+console.log(calcArea(2,50));
+
+
+//Exercicio 3
+function splitSentence(string){
+    let novaString = string.split(" ");
+    return novaString; 
+}
+console.log(splitSentence("Banana doce"));
+
+
+//Exercício 4
+function concatName(string){
+    
+}
