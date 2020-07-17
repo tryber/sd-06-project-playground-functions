3997be70977665445042c4e762588556e2ef1e1d
Alessandra <itsalematos@hotmail.com>
2020-07-17 14:32:27 -0400

    Add correção primeiro exercício

--- src/challenges.js
+++ src/challenges.js
@@ -2,13 +2,13 @@
 
 // Desafio 1
 function compareTrue(valor1, valor2) {
-  if(valor1 === valor2){
+  if(valor1 === valor2 && valor2 === valor1){
     return true;
   } else {
     return false;
   }
 }
-console.log(compareTrue(10,10));
+console.log(compareTrue(10,6));
 
 // Desafio 2
 function calcArea() {
