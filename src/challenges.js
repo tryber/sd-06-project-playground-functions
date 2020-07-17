f86537ae9f6f663571dd2952f823aaa3a84e5ac5
jederson <jmacedo.ti@gmail.com>
2020-07-17 14:46:07 -0300

    Exercício 03

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentence) {
+  let result = sentence.split(' ');
+  return result;
 }
 
 // Desafio 4
