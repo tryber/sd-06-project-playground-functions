94662cd935b1fae820e529ca97b958b22086c205
Pedro Schil <schil.ambiental@gmail.com>
2020-07-17 19:38:16 -0300

    inserindo Math.abs no gato caça rato

--- src/challenges.js
+++ src/challenges.js
@@ -52,10 +52,10 @@ function highestCount() {
 // Desafio 7
 function catAndMouse(mouse,cat1,cat2) {
   // seu código aqui
-if(mouse-cat1 > mouse-cat2 ){
+if(Math.abs(mouse-cat1) > Math.abs(mouse-cat2) ){
   return "cat2"}
 else if(
-  mouse-cat1 < mouse-cat2)
+  Math.abs(mouse-cat1) < Math.abs(mouse-cat2))
   {return "cat1"
 }else{return "os gatos trombam e o rato foge"}
 }
