7c539ca44703f2dd89d8fac55f35710dfdfe819f
Zeca Costa <jccostaso@gmail.com>
2020-07-18 05:39:51 -0300

    Atualizaçao para incluir desafio #4

--- src/challenges.js
+++ src/challenges.js
@@ -1,43 +1,53 @@
 // Desafio 1
-//JavaScript possui um operador lógico &&, o qual recebe dois valores e retorna true se ambos os valores são verdadeiros, e retorna false se algum dos valores não o for.
-//Considerando isso, crie uma função chamada compareTrue que, ao receber dois booleanos:
-//Retorne true se ambos os valores são verdadeiros;
-//Retorne false se um ou ambos os parâmetros forem falsos.
-//Faça a função utilizando o operador &&.
+
+// JavaScript possui um operador lógico &&, o qual recebe dois valores e retorna true se ambos os valores são verdadeiros, e retorna false se algum dos valores não o for.
+// Considerando isso, crie uma função chamada compareTrue que, ao receber dois booleanos:
+// Retorne true se ambos os valores são verdadeiros;
+// Retorne false se um ou ambos os parâmetros forem falsos.
+// Faça a função utilizando o operador &&.
 
 function compareTrue(value1, value2) {
   return value1 && value2;
 }
 
-console.log(compareTrue (false, false));
-
-//Escreva uma função com o nome calcArea que receba um valor de base (chamado base) e outro de altura (chamado height) de um triângulo e retorne o cálculo da sua área.
-
-//Lembre-se que a área de um triângulo é calculada através da seguinte fórmula: (base * altura) / 2.
+console.log(compareTrue(false, false));
 
 // Desafio 2
+
+// Escreva uma função com o nome calcArea que receba um valor de base (chamado base) e outro de altura (chamado height) de um triângulo e retorne o cálculo da sua área.
+
+// Lembre-se que a área de um triângulo é calculada através da seguinte fórmula: (base * altura) / 2.
+
 function calcArea(base, height) {
   return (base * height) / 2;
 }
 
-console.log(calcArea (5, 4));
+console.log(calcArea(5, 4));
 
 // Desafio 3
-//Escreva uma função com o nome splitSentence, a qual receberá uma string e retornará uma array de strings separadas por cada espaço na string original.
 
-//Exemplo: se a função receber a string "go Trybe", o retorno deverá ser ['go', 'Trybe'].
+// Escreva uma função com o nome splitSentence, a qual receberá uma string e retornará uma array de strings separadas por cada espaço na string original.
+
+// Exemplo: se a função receber a string "go Trybe", o retorno deverá ser ['go', 'Trybe'].
 
 function splitSentence(word) {
-  return  word.split(" ");
+  return word.split(' ');
 }
 
-console.log(splitSentence("Hello World"));
-
+console.log(splitSentence('Hello World'));
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+
+/*Escreva uma função com o nome concatName que, ao receber uma array de strings, retorne uma string com o formato 'ÚLTIMO ITEM, PRIMEIRO ITEM', independente do tamanho da array.
+
+Isso quer dizer que, caso o parâmetro passado para concatName seja a Array ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'], a função deverá retornar Paolillo, Lucas.*/
+
+function concatName(newArray) {
+  return newArray[0] + ', '+ newArray[newArray.length -1];
 }
+
+let testArray = ['Pedrina', 'Maria', 'Julieta', 'Catarina', 'Marieta'];
+console.log(concatName(testArray));
 
 // Desafio 5
 function footballPoints() {
@@ -104,3 +114,5 @@ module.exports = {
   splitSentence,
   triangleCheck,
 }
+
+let arr
