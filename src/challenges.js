791c218cf888af5604110fb13ec48788875e93c5
Havyner Caetano <hrcgeo@gmail.com>
2020-07-18 13:02:52 -0300

    Fix singular or plural in return message

--- src/challenges.js
+++ src/challenges.js
@@ -218,7 +218,7 @@ function hydrate(order) {
     glassesOfWater += parseInt(glassesOfAlcoholicDrinks[glasses]);
   }
 
-  return `${glassesOfWater} copos de água`;
+  return glassesOfWater <= 1 ? `${glassesOfWater} copo de água` : `${glassesOfWater} copos de água`;
 }
 
 module.exports = {
