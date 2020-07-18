40e42589a01f1781b48a63def77a307708680abd
Vagner Pereira <sousa.vagner82@gmail.com>
2020-07-18 12:36:37 -0300

    Alteração exercicio 10

--- src/challenges.js
+++ src/challenges.js
@@ -145,7 +145,7 @@ function techList(tech, name) {
   let techSort = tech.sort();
   let tecnologias = [];
   for(index in techSort){
-    tecnologias.push(techSort[index] = {tech: techSort[index], name :"Vagner",});
+    tecnologias.push(techSort[index] = {tech: techSort[index], nome : name,});
   }
   if(tecnologias.length === 0){
     return 'Vazio!';
