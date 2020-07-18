eac092a2b0f19be0e21960c946abb34301c6cb7a
STEFANO-BRANZ <stefano.branz@gmail.com>
2020-07-18 20:36:49 -0300

    Requisite 7 some last changes 3

--- src/challenges.js
+++ src/challenges.js
@@ -68,7 +68,7 @@ function catAndMouse(mouse, cat1, cat2) {
     return "cat2";
   }
   else if (distanceCat1 === distanceCat2) {
-    return ("Os gatos trombam e o rato foge");
+    return ("os gatos trombam e o rato foge");
   }
 }
 
