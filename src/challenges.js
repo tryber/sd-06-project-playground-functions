91607dc990dcaf54535af684eaaa2b8ad99b2ccc
bragammartins <bragammartins@gmail.com>
2020-07-17 21:44:38 -0300

    3 atividade - 3 - correcting codeclimate errors

--- src/challenges.js
+++ src/challenges.js
@@ -21,7 +21,7 @@ function compareTrue(value1, value2) {
 // console.log(compareTrue(value1, value2))
 
 // Desafio 2
-/* 2 - Área do triângulo Escreva uma função com o nome calcArea que receba um valor de base 
+/* 2 - Área do triângulo Escreva uma função com o nome calcArea que receba um valor de base
 (chamado base) e outro de altura (chamado height) de um triângulo e retorne o cálculo da sua área.
 Lembre-se que a área de um triângulo é calculada através da seguinte fórmula: (base * altura) / 2.
 */
@@ -33,7 +33,7 @@ function calcArea(base, height) {
 }
 // console.log (calcArea(base,height));
 /* 3 - Dividindo a frase
-  Escreva uma função com o nome splitSentence, a qual receberá uma string e retornará uma array 
+  Escreva uma função com o nome splitSentence, a qual receberá uma string e retornará uma array
   de strings separadas por cada espaço na string original.
 
   Exemplo: se a função receber a string "go Trybe", o retorno deverá ser ['go', 'Trybe'].
@@ -41,17 +41,17 @@ function calcArea(base, height) {
 // Desafio 3
 // let frase = 'lilica linda';
 function splitSentence(frase) {
-  return frase.split('', ) ;
+  return frase.split(' ',);
 
   // seu código aqui
 }
 // console.log(splitSentence(frase));
 // Desafio 4
 /* 4 - Concatenação de strings
-  Escreva uma função com o nome concatName que, ao receber uma array de strings, retorne uma 
+  Escreva uma função com o nome concatName que, ao receber uma array de strings, retorne uma
   string com o formato 'ÚLTIMO ITEM, PRIMEIRO ITEM', independente do tamanho da array.
 
-  Isso quer dizer que, caso o parâmetro passado para concatName seja a Array ['Lucas', 
+  Isso quer dizer que, caso o parâmetro passado para concatName seja a Array ['Lucas',
   'Cassiano', 'Ferraz', 'Paolillo'], a função deverá retornar Paolillo, Lucas.
 */
 function concatName() {
