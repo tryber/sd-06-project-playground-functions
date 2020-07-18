96b26468d8be1e133eda23fc4398eb5a43a31617
STEFANO-BRANZ <stefano.branz@gmail.com>
2020-07-18 20:33:04 -0300

    Requisite 7 some last changes 2

--- src/challenges.js
+++ src/challenges.js
@@ -62,10 +62,10 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanceCat1 = Math.abs(cat1 - mouse);
   let distanceCat2 = Math.abs(cat2 - mouse);
   if (distanceCat1 < distanceCat2) {
-    return cat1;
+    return "cat1";
   }
   else if (distanceCat2 < distanceCat1) {
-    return cat2;
+    return "cat2";
   }
   else if (distanceCat1 === distanceCat2) {
     return ("Os gatos trombam e o rato foge");
