25261ca68ff848acfe1083dc5f41213252009513
FPhilipi <fabiphilipi@gmail.com>
2020-07-17 21:21:57 -0300

    Requisito 8

--- src/challenges.js
+++ src/challenges.js
@@ -68,9 +68,26 @@ function catAndMouse(mouse, cat1, cat2) {
 console.log(catAndMouse(1, 2, 3));
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let arrayOutPut = [];
+  let newInPut;    
+    for (let index in array) {
+       if (array[index] % 3 === 0 && array[index] % 5 !== 0) {
+            newInPut = "fizz";
+        } else if (array[index] % 3 !== 0 && array[index] % 5 === 0){
+            newInPut = "buzz";
+        } else if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+            newInPut = "fizzBuzz";
+        } else {
+            newInPut = "bug!";
+        }
+        arrayOutPut.push(newInPut);
+        newInPut = ""; 
+        }
+    return arrayOutPut;    
 }
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
+
 
 // Desafio 9
 function encode() {
