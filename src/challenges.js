27b56df74b778041dc64794ac8557dae175d1695
Daniel Jesus <dev.danieljs@gmail.com>
2020-07-17 22:53:19 -0300

    Changing challenge 6

--- src/challenges.js
+++ src/challenges.js
@@ -39,11 +39,19 @@ let position = 0,
 
 function highestCount(array) {
   for (let i = 0; i < array.length; i += 1) {
-    if (array[position] <= array[i]) {
+    if (array[position] < array[i]) {
       position = i;
+    }
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[position] == array[i]) {
       cont += 1;
     }
   }
+  return cont;
+}
+
 /*
   for (let newArray in array) {
     if (array[position] <= array[newArray]) {
@@ -52,10 +60,6 @@ function highestCount(array) {
     }
   }
 */
-  return cont;
-}
-// let sum = [0, 4, 4, 4, 9, 2, 1];
-// console.log(highestCount(sum));
 
 
 // Desafio 7
@@ -99,12 +103,59 @@ console.log(fizzBuzz(numbers));
 
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+/*
+Crie duas funções: a primeira deverá se chamar encode e, ao receber uma string como parâmetro, deverá trocar todas as vogais minúsculas por números, de acordo com o formato a seguir:
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+Ou seja, caso o parâmetro de encode seja "hi there!", o retorno deverá ser "h3 th2r2!".
+A segunda função deverá se chamar decode e faz o contrário de encode - ou seja, recebe uma string contendo números no lugar de letras minúsculas e retornará uma string com vogais minúsculas no lugar dos números (então, caso o parâmetro de decode seja "h3 th2r2!", o retorno deverá ser "hi there!").
+*/
+function encode(string) {
+  let word = '';
+  for (let i = 0; i < string.length; i += 1) {
+      if (string[i] === 'a') {
+        word[i] = '1';
+      } else if (string[i] === 'e') {
+        word[i] = '2';
+      } else if (string[i] === 'i') {
+        word[i] = '3';
+      } else if (string[i] === 'o') {
+        word[i] = '4';
+      } else if (string[i] === 'u') {
+        word[i] = '5';
+      } else {
+        word += string[i];
+      }
+  }
+  return word;
 }
-function decode() {
-  // seu código aqui
+// let phrase = 'hi there!';
+// console.log(encode(phrase));
+
+function decode(string) {
+  for (let i = 0; i < string.length; i += 1) {
+    if (string[i] === '1') {
+      word[i] = 'a';
+    } else if (string[i] === '2') {
+      word[i] = 'e';
+    } else if (string[i] === '3') {
+      word[i] = 'i';
+    } else if (string[i] === '4') {
+      word[i] = 'o';
+    } else if (string[i] === '5') {
+      word[i] = 'u';
+    } else {
+      word += string[i];
+    }
+  }
+  return word;
 }
+// let phrase = 'h3 th2r2!';
+// console.log(decode(phrase));
+
 
 // Desafio 10
 function techList() {
