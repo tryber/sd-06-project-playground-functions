5fba7ba60526208c9926b1150b757cb875070352
Igor Leão <igorleao.eng@gmail.com>
2020-07-18 11:21:05 -0300

    codeclimate 5

--- src/challenges.js
+++ src/challenges.js
@@ -28,17 +28,13 @@ function concatName(d) {
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let points = 0;
-  wins = wins * 3;
-  points = wins + ties;
-  return points;
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
 function highestCount(array) {
   let maior = array[0];
   let cont = 0;
-  
   for (let i = 0; i < array.length; i += 1) {
     if (array[i] === maior) {
       cont += 1;
@@ -55,91 +51,99 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
   let dist1 = 0;
   let dist2 = 0;
-  dist1 = cat1-mouse;
+  dist1 = cat1 - mouse;
   dist1 = Math.abs(dist1);
-  dist2 = cat2-mouse;
+  dist2 = cat2 - mouse;
   dist2 = Math.abs(dist2);
   if (dist1 > dist2) {
     return 'cat2';
   }
-  if ( dist1 === dist2 ) {
-    return 'os gatos trombam e o rato foge';
-  }
   if (dist1 < dist2) {
     return 'cat1';
   }
+    return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
-function fizzBuzz(array_1) {
-  let array_2 = [];
-  for (let i = 0; i < array_1.length; i += 1) {
-    if (array_1[i] % 3 === 0 && array_1[i] % 5 !== 0) {
-      array_2[i] = 'fizz';
+function fizzBuzz(vetNumbers) {
+  let array2 = [];
+  for (let i = 0; i < vetNumbers.length; i += 1) {
+    if (vetNumbers[i] % 3 === 0 && vetNumbers[i] % 5 != 0) {
+      array2[i] = 'fizz';
     }
-    if (array_1[i] % 5 === 0 && array_1[i] % 3 !== 0) {
-      array_2[i] = 'buzz';
+    else if (vetNumbers[i] % 5 === 0 && vetNumbers[i] % 3 != 0) {
+      array2[i] = 'buzz';
     }
-    if (array_1[i] % 5 === 0 && array_1[i] % 3 === 0) {
-      array_2[i] = 'fizzbuzz';
+    else if (vetNumbers[i] % 5 === 0 && vetNumbers[i] % 3 === 0) {
+      array2[i] = 'fizzbuzz';
     }
     else {
-      array_2[i] = 'bug';
+      array2[i] = 'bug';
     }
   }
-  return array_2;
+  return array2;
 }
 
 // Desafio 9
-function encode(word_2) {
+function encode(word2) {
   const vogais = {
-    a : 1,
-    e : 2,
-    i : 3,
-    o : 4,
-    u : 5,
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
   };
-  let vet_1 = [];
-  
-  for (let i = 0; i < word_2.length; i += 1) {
-    let char =word_2.charAt(i);
-    if (char in vogais){
-      vet_1.push(vogais[char]);
+  let vet1 = [];
+  for (let i = 0; i < word2.length; i += 1) {
+    let char = word2.charAt(i);
+    if (char in vogais) {
+      vet1.push(vogais[char]);
     }
     else {
-      vet_1.push([char]);
+      vet1.push([char]);
     }
   }
-  let retorno = vet_1.join('');
+  let retorno = vet1.join('');
   return retorno;
 }
 
-function decode(word_3) {
+function decode(word3) {
   const numbers = {
-    1 : 'a',
-    2 : 'e',
-    3 : 'i',
-    4 : 'o',
-    5 : 'u',
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
   };
-  let vet_1 = [];
-  
-  for (let i = 0; i < word_3.length; i += 1) {
-    let char = word_3.charAt(i);
-    if(char in numbers){
-      vet_1.push(numbers[char]);
+  let vet1 = [];
+  for (let i = 0; i < word3.length; i += 1) {
+    let char = word3.charAt(i);
+    if (char in numbers) {
+      vet1.push(numbers[char]);
     }
     else {
-      vet_1.push([char]);
+      vet1.push([char]);
     }
   }
-  let retorno = vet_1.join('');
+  let retorno = vet1.join('');
   return retorno;
 }
 
 // Desafio 10
 function techList() {
   // seu código aqui
+  function techList(array1, name) {
+    let sort = array1.sort();
+    let final = [];
+    for(i in sort){
+      final.push({"tech" : sort[i], "name" : name});
+    }
+    if(array1.length === 0){
+      return "Vazio!";
+    } else {
+      return final;
+    }
+  }
 }
 
 // Desafio 11
