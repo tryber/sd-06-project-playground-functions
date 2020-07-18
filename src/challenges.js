2faa4571ed7280b84a95060c12bd83114442fadd
Micaeli Teixeira <luime6@gmail.com>
2020-07-17 23:12:08 -0300

    Resolvendo desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -90,7 +90,7 @@ function fizzBuzz(arrayNew) {
   
   let newFizz = [];
 
-  for (i = 0; i<arrayNew.length; i++){
+  for (i = 0; i<arrayNew.length; i +=1 ){
     if ((arrayNew[i]%3==0) && (arrayNew[i]%5!==0)){
       newFizz[i] = 'fizz';
     }else if ((arrayNew[i]%5==0) && (arrayNew[i]%3!==0)){
@@ -106,12 +106,63 @@ return newFizz;
 
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+/*Crie duas funções: a primeira deverá se chamar encode e, ao receber uma string como parâmetro, deverá trocar todas as vogais minúsculas por números, de acordo com o formato a seguir:
+
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+
+Ou seja, caso o parâmetro de encode seja "hi there!", o retorno deverá ser "h3 th2r2!".
+
+A segunda função deverá se chamar decode e faz o contrário de encode - ou seja, recebe uma string contendo números no lugar de letras minúsculas e retornará uma string com vogais minúsculas no lugar dos números (então, caso o parâmetro de decode seja "h3 th2r2!", o retorno deverá ser "hi there!").*/
+
+function encode(vowels) {
+    
+    let newVowels = vowels.split('');
+    
+    for (let i = 0; i < newVowels.length; i+= 1){
+     if (newVowels[i]==='a'){
+        newVowels[i]= '1';
+     }else if (newVowels[i]==='e'){
+        newVowels[i]= '2';
+     }else if (newVowels[i]==='i'){
+        newVowels[i]= '3';
+     }else if (newVowels[i]==='o'){
+        newVowels[i]= '4';
+     }else if (newVowels[i]==='u'){
+        newVowels[i]= '5';
+     }else {
+        newVowels[i]= newVowels[i];
+     }
 }
-function decode() {
-  // seu código aqui
+  return newVowels.join('') ;
 }
+
+
+function decode(noVowels) {
+  
+  let newVowelsNo = noVowels.split('');
+    
+  for (let i = 0; i < newVowelsNo.length; i+= 1){
+   if (newVowelsNo[i]==='1'){
+      newVowels[i]= 'a';
+   }else if (newVowelsNo[i]==='2'){
+      newVowelsNo[i]= 'e';
+   }else if (newVowelsNo[i]==='3'){
+      newVowelsNo[i]= 'i';
+   }else if (newVowelsNo[i]==='4'){
+      newVowelsNo[i]= 'o';
+   }else if (newVowelsNo[i]==='5'){
+      newVowelsNo[i]= 'u';
+   }else {
+      newVowelsNo[i]= newVowelsNo[i];
+   }
+}
+return newVowelsNo.join('') ;
+}
+
 
 // Desafio 10
 function techList() {
