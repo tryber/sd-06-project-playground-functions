ac8dd504b6868abe89b6cec2f62bd6f9ac180f3c
ricknois <rickssti@outlook.com>
2020-07-17 23:33:48 -0300

    Erro

--- src/challenges.js
+++ src/challenges.js
@@ -4,12 +4,12 @@ function compareTrue(val1, val2) {
     return true;
   } else {
     return false;
-  } 
+  }
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  let calc = (base * height)/2;
+  let calc = (base * height) / 2;
   return calc;
 
 
@@ -23,7 +23,7 @@ function splitSentence(str) {
 // Desafio 4
 function concatName(array) {
   for (const key in array) {
-    return array[array.length - 1] +", "+ array[0];
+    return array[array.length - 1] + ", " + array[0];
   }
 }
 
@@ -36,14 +36,14 @@ function footballPoints(wins, ties) {
 function highestCount(array) {
   let hNumber = 0;
   for (let index = 0; index < array.length; index += 1) {
-    if (array[index] > hNumber){
-    hNumber = array[index]
-    } 
+    if (array[index] > hNumber) {
+      hNumber = array[index]
+    }
   }
   let aux = 0;
   for (const key in array) {
     if (hNumber === array[key]) {
-    aux += 1 
+      aux += 1
     }
   }
   return aux;
@@ -55,7 +55,7 @@ function catAndMouse(mouse, cat1, cat2) {
   let posC1, posC2, posMouse;
   if (cat1 <= mouse) {
     posC1 = mouse - cat1;
-  }else{
+  } else {
     posC1 = cat1 - mouse;
   }
   if (cat2 <= mouse) {
@@ -65,9 +65,9 @@ function catAndMouse(mouse, cat1, cat2) {
   }
   if (posC1 > posC2) {
     return "cat2"
-  } else if(posC2 > posC1){
+  } else if (posC2 > posC1) {
     return "cat1"
-  } else{
+  } else {
     return "os gatos trombam e o rato foge"
   }
 }
@@ -75,13 +75,13 @@ function catAndMouse(mouse, cat1, cat2) {
 function fizzBuzz(array) {
   array2 = [];
   for (const key in array) {
-    if (array[key]%3 === 0 && array[key] % 5 === 0){
+    if (array[key] % 3 === 0 && array[key] % 5 === 0) {
       array2.push('fizzBuzz')
-    }else if (array[key] % 5 === 0){
+    } else if (array[key] % 5 === 0) {
       array2.push('buzz');
-    }else if (array[key] % 3 === 0){
+    } else if (array[key] % 3 === 0) {
       array2.push('fizz')
-    }else{
+    } else {
       array2.push('bug!')
     }
   }
@@ -91,45 +91,45 @@ function fizzBuzz(array) {
 // Desafio 9
 function encode(str) {
   let aux = str.split("");
-  let aux2 = []; 
+  let aux2 = [];
   for (const key in aux) {
     if (aux[key] === 'a') {
       aux2 = aux2 + '1';
-    }else if(aux[key] === 'e'){
+    } else if (aux[key] === 'e') {
       aux2 += '2';
-    }else if (aux[key] === 'i'){
+    } else if (aux[key] === 'i') {
       aux2 = aux2 + '3';
-    }else if (aux[key] === 'o'){
+    } else if (aux[key] === 'o') {
       aux2 += '4';
-    }else if (aux[key] === 'u'){
+    } else if (aux[key] === 'u') {
       aux2 += '5'
-    }else{
+    } else {
       aux2 += aux[key];
     }
   }
   return aux2;
 }
 
-function decode(str) {
-  let aux = str.split("");
-  let aux2 = []; 
-  for (const key in aux) {
-    if (aux[key] === '1') {
-      aux2 = aux2 + 'a';
-    }else if(aux[key] === '2'){
-      aux2 += 'e';
-    }else if (aux[key] === '3'){
-      aux2 = aux2 + 'i';
-    }else if (aux[key] === '4'){
-      aux2 += 'o';
-    }else if (aux[key] === '5'){
-      aux2 += 'u'
-    }else{
-      aux2 += aux[key];
+function decode(string) {
+  let split = string.split("");
+  let conca = [];
+  for (const key in split) {
+    if (split[key] === '1') {
+      conca = conca + 'a';
+    } else if (split[key] === '2') {
+      conca += 'e';
+    } else if (split[key] === '3') {
+      conca = conca + 'i';
+    } else if (split[key] === '4') {
+      conca += 'o';
+    } else if (split[key] === '5') {
+      conca += 'u'
+    } else {
+      conca += split[key];
     }
   }
-  return aux2;
-  
+  return conca;
+
 }
 console.log(decode("h3 th2r2"))
 
@@ -139,16 +139,15 @@ function techList() {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-}
+function generatePhoneNumber() {}
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC)){
+  if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC)) {
     return true;
-  }else if(lineB < (lineC + lineA) && lineB > Math.abs(lineA - lineC)){
+  } else if (lineB < (lineC + lineA) && lineB > Math.abs(lineA - lineC)) {
     return true;
-  }else if(lineC < (lineB + lineA) && lineC > Math.abs(lineA - lineB)){
+  } else if (lineC < (lineB + lineA) && lineC > Math.abs(lineA - lineB)) {
     return true;
   } else {
     return false;
