9dae6baf5dde5c03f31fb6a52cf7daf0ed49a286
Stefano Branz <stefano.branz@gmail.com>
2020-07-18 00:27:36 -0300

    Requirement four first push

--- src/challenges.js
+++ src/challenges.js
@@ -28,9 +28,17 @@ function splitSentence(string) {
   return splitedPhrase;
 }
 
+// Escreva uma função com o nome concatName que, ao receber uma array de strings, 
+// retorne uma string com o formato 'ÚLTIMO ITEM, PRIMEIRO ITEM', independente do tamanho da array.
+
+// Isso quer dizer que, caso o parâmetro passado para concatName seja a 
+// Array ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'], a função deverá retornar Paolillo, Lucas.
+
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  for (let index = 0; index < array.length; index += 1){
+    return newArray = [array[array.length - 1], array[0]];
+  }
 }
 
 // Desafio 5
