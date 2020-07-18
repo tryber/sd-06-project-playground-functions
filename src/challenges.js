941bc18f8011f95041ca0fe83399d999e29676c8
STEFANO-BRANZ <stefano.branz@gmail.com>
2020-07-18 20:29:51 -0300

    Requisite 7 some last changes

--- src/challenges.js
+++ src/challenges.js
@@ -57,18 +57,10 @@ function highestCount(array) {
   return highestNumberCount;
 }
 
-// Imagine que existem dois gatos, os quais chamaremos de cat1 e cat2, e que ambos estão atrás de um rato chamado mouse. Imagine que cada um dos três animais está em uma posição representada por um número.
-
-// Sabendo disso, crie uma função chamada catAndMouse que, ao receber a posição de mouse, cat1 e cat2, nessa ordem, calcule as distâncias entre o rato e os gatos e retorne qual dos felinos irá alcançar o rato primeiro (sendo aquele que estará mais perto).
-
-// Exemplo: caso o gato cat2 esteja a 2 unidades de distância do rato, e cat1 esteja a 3 unidades, sua função deverá retornar cat2.
-
-// Caso os gatos estejam na mesma distância do rato, a função deverá retornar a string "os gatos trombam e o rato foge".
-
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let distanceCat1 = math.abs(cat1 - mouse);
-  let distanceCat2 = math.abs(cat2 - mouse);
+  let distanceCat1 = Math.abs(cat1 - mouse);
+  let distanceCat2 = Math.abs(cat2 - mouse);
   if (distanceCat1 < distanceCat2) {
     return cat1;
   }
