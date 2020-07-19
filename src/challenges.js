6091729221ff0388ed79591521c4d42a7cf40934
STEFANO-BRANZ <stefano.branz@gmail.com>
2020-07-19 15:02:46 -0300

    Requisite 10 return vazio fix try

--- src/challenges.js
+++ src/challenges.js
@@ -149,13 +149,11 @@ function techList(techArray, name) {
   if (newTechArray == 0) {
     return "Vazio!";
   }
-  else{
   for (i = 0; i < sortedTechArray.length; i += 1) {
     let tech = sortedTechArray[i];
     newTechArray.push ({tech, name,});
   }
   return newTechArray;
-  }
 }
 
 // Desafio 11
