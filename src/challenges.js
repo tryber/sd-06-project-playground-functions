952b337fb1f2c9feae2ab484dd1b6a62cd311832
Vagner Pereira <sousa.vagner82@gmail.com>
2020-07-19 19:13:23 -0300

    Corrigindo CodeClimate

--- src/challenges.js
+++ src/challenges.js
@@ -160,7 +160,6 @@ function generatePhoneNumber(number) {
   numberFormat += '-' + number[7] + number[8] + number[9] + number[10];
   return numberFormat;
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if ((Math.abs(lineB - lineC) > lineA || lineA > lineB + lineC)) {
