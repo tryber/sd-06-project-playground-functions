14e58a286676eeb75f9998188b887537a195b5dc
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-19 19:06:00 -0300

    Adding exercise 12 (done)

--- src/challenges.js
+++ src/challenges.js
@@ -216,8 +216,18 @@ console.log(generatePhoneNumber([9, 2, 3, 0, 5, -6, 7, 8, -7, 0, 1, 10]))
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
-  //if (lineA < (lineB + lineC && lineA >  ))
+  let triangleReturn = false;
+  if (lineA < (lineB + lineC) && lineA > (Math.abs(lineB) - (Math.abs(lineC)))) {
+    triangleReturn = true;
+  } else if (lineB < (lineA + lineC) && lineB > (Math.abs(lineA) - (Math.abs(lineC)))) {
+    triangleReturn = true;
+  } else if (lineC < (lineA + lineB) && lineC > (Math.abs(lineA) - (Math.abs(lineB)))) {
+    triangleReturn = true;
+  }
+  return triangleReturn;
 }
+
+console.log(triangleCheck(10, 14, 8))
 
 // Desafio 13
 function hydrate() {
