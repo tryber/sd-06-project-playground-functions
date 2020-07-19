10884bdc1f68626822d61d8dc459121fff0cf290
Flavio Oliveira <flavsoliver@gmail.com>
2020-07-19 01:57:40 -0300

    enviando  desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -88,9 +88,30 @@ function highestCount(numbersDesafio6) {
 
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+// Imagine que existem dois gatos, os quais chamaremos de cat1 e cat2
+// e que ambos estão atrás de um rato chamado mouse
+// Imagine que cada um dos três animais está em uma posição
+// representada por um número
+// Crie uma função chamada catAndMouse que, ao receber a posição
+// de mouse, cat1 e cat2, nessa ordem, calcule as distâncias entre o rato
+// e os gatos e retorne qual dos felinos irá alcançar o rato primeiro
+// sendo aquele que estará mais perto
+// Caso os gatos estejam na mesma distância do rato
+// a função deverá retornar a string "os gatos trombam e o rato foge"
+function catAndMouse(cat1, cat2, mouse) {
+  let killMouse = ('');
+  let positionCat1 = cat1 - mouse;
+  let positionCat2 = cat2 - mouse;
+  if (positionCat1 - positionCat2 === 0) {
+    return killMouse = ('os gatos trombam e o rato foge');
+  } else if (positionCat1 < positionCat2 - mouse) {
+    return killMouse = ('cat1');
+  } return killMouse = ('cat2');
 }
+
+// console.log('<---- Desafio 6 ---->');
+console.log(catAndMouse(30, 3, 0));
+
 
 // Desafio 8
 function fizzBuzz() {
