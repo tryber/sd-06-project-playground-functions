be307a5420d3147cdc890d23a591cfe85cfab5f3
Vagner Pereira <sousa.vagner82@gmail.com>
2020-07-18 23:43:31 -0300

    Verificação codeclimate

--- src/challenges.js
+++ src/challenges.js
@@ -3,7 +3,7 @@ function compareTrue(param1, param2) {
   if (param1 && param2) {
     return true;
   }
-    return false;
+  return false;
 }
 
 // Desafio 2
@@ -18,7 +18,7 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(nomeCompleto) {
-  let nome = nomeCompleto[nomeCompleto.length-1] + ', ' + nomeCompleto[0];
+  let nome = nomeCompleto[nomeCompleto.length - 1] + ', ' + nomeCompleto[0];
   return nome;
 }
 
@@ -36,7 +36,7 @@ function highestCount(numeros) {
       maior = numeros[index];
     }
   }
-  for (index in numeros) {
+  for (let index in numeros) {
     if (maior === numeros[index]) {
       cont += 1;
     }
@@ -59,29 +59,28 @@ function catAndMouse(mouse, cat1, cat2) {
     dist2 = mouse - cat2;
   }
   if (dist1 < dist2) {
-    return "cat1";
+    return 'cat1';
   } else
   if (dist2 < dist1) {
-    return "cat2";
-  } else {
-    return "os gatos trombam e o rato foge";
+    return 'cat2';
   }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
 function fizzBuzz(numeros) {
-  let result =[];
-  for (index in numeros) {
+  let result = [];
+  for (let index in numeros) {
     if (numeros[index] % 3 === 0 && numeros[index] % 5 !== 0) {
-      result.push("fizz");
+      result.push('fizz');
     } else
     if (numeros[index] % 3 !== 0 && numeros[index] % 5 === 0) {
-      result.push("buzz");
+      result.push('buzz');
     } else
     if (numeros[index] % 3 === 0 && numeros[index] % 5 === 0) {
-      result.push("fizzBuzz");
+      result.push('fizzBuzz');
     } else {
-      result.push("bug!");
+      result.push('bug!');
     }
   }
   return result;
@@ -90,7 +89,7 @@ function fizzBuzz(numeros) {
 // Desafio 9
 function encode(str) {
   let aux = str.split('');
-  for (index in aux) {
+  for (let index in aux) {
     if (aux[index] === 'a') {
       aux[index] = '1';
     } else
@@ -108,14 +107,14 @@ function encode(str) {
     }
   }
   str = '';
-  for (let index in aux) {
-    str += aux[index];
+  for (let i = 0; i < aux.length; i += 1) {
+    str += aux[i];
   }
   return str;
 }
 function decode(str) {
   let aux = str.split('');
-  for (index in aux) {
+  for (let index in aux) {
     if (aux[index] === '1') {
       aux[index] = 'a';
     } else
@@ -133,21 +132,21 @@ function decode(str) {
     }
   }
   str = '';
-  for (index in aux) {
-    str += aux[index];
+  for (let i = 0; i < aux.length; i += 1) {
+    str += aux[i];
   }
   return str;
 }
 
 // Desafio 10
-function techList(techList, name) {
-  if (techList.length === 0) {
+function techList(tecno, name) {
+  if (tecno.length === 0) {
     return 'Vazio!';
   }
   let tecnologias = [];
-  techList.sort();
-  for (index in techList) {
-    let tech = techList[index];
+  tecno.sort();
+  for (let i = 0; i < tecno.length; i += 1) {
+    let tech = tecno[i];
     tecnologias.push({ tech, name });
   }
   return tecnologias;
@@ -158,27 +157,27 @@ function generatePhoneNumber(number) {
   let cont = 0;
   let numberFormat = '(';
   if (number.length !== 11) {
-    return "Array com tamanho incorreto.";
+    return 'Array com tamanho incorreto.';
   }
   for (let i = 0; i < number.length; i += 1) {
     if (i === 2) {
       numberFormat += ') ' + number[i];
     } else
-    if (i === 7){
+    if (i === 7) {
       numberFormat += '-' + number[i];
     } else {
       numberFormat += number[i]
     }
     if (number[i] < 0 || number[i] > 9) {
-      return "não é possível gerar um número de telefone com esses valores";
+      return 'não é possível gerar um número de telefone com esses valores';
     }
     for (let l = 0; l < number.length; l += 1) {
       if (number[i] === number[l]) {
-        cont +=1;
+        cont += 1;
       }
     }
     if (cont > 2) {
-      return "não é possível gerar um número de telefone com esses valores";
+      return 'não é possível gerar um número de telefone com esses valores';
     }
     cont = 0;
   }
@@ -190,28 +189,25 @@ function triangleCheck(lineA, lineB, lineC) {
   if ((Math.abs(lineB - lineC) > lineA || lineA > lineB + lineC)
   || (Math.abs(lineA - lineC) > lineB || lineB > lineA + lineC)
   || (Math.abs(lineB - lineA) > lineC || lineC > lineB + lineA)) {
-      return false;
-    } else {
-      return true;
-    }
+    return false;
+  }
+  return true;
 }
-
 
 // Desafio 13
 function hydrate(drinks) {
   let num = drinks.match(/\d+/g).map(Number);
   let cont = 0;
   for (let index in num) {
-    if (parseInt(num[index]) < 1 || parseInt(num[index]) > 9) {
+    if (parseInt(num[index], radix) < 1 || parseInt(num[index], radix) > 9) {
       return 'número inválido!'
     }
-    cont += parseInt(num[index]);
+    cont += parseInt(num[index], radix);
   }
   if (cont === 1) {
-    return cont + " copo de água";
-  } else {
-    return cont + " copos de água";
+    return cont + ' copo de água';
   }
+  return cont + ' copos de água';
 }
 
 module.exports = {
