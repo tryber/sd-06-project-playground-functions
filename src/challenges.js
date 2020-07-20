446fe4afe994ac8fc207b3f0bae04a80993f51aa
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-07-19 21:38:20 -0300

    Correção do desafio generatePhoneNumber

--- src/challenges.js
+++ src/challenges.js
@@ -193,7 +193,7 @@ function generatePhoneNumber(n) {
   } else if (checkNumbers(n)) {
     result = 'não é possível gerar um número de telefone com esses valores';
   } else {
-    result = `(${n[0]}${n[1]}) ${n[2]}${n[3]}${n[4]}${n[5]}${n[6]} - ${n[7]}${n[8]}${n[9]}${n[10]}`;
+    result = `(${n[0]}${n[1]}) ${n[2]}${n[3]}${n[4]}${n[5]}${n[6]}-${n[7]}${n[8]}${n[9]}${n[10]}`;
   }
   return result;
 }
