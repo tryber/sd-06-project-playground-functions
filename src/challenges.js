ddf01551b920772860d4e4f1df452c9529485449
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-07-20 11:27:51 -0300

    Implementation of tests

--- src/challenges.js
+++ src/challenges.js
@@ -100,15 +100,15 @@ function fizzBuzz(numArray) {
 // Desafio 9
 function encode(strEntry) {
   // seu código aqui
-  return encodeOrDecode(stEntry, 'encode');
+  return encodeOrDecode(strEntry, 'encode');
 }
 function decode(strEntry) {
   // seu código aqui
-  return encodeOrDecode(stEntry, 'decode');
+  return encodeOrDecode(strEntry, 'decode');
 }
 
-function decodeOrEncode (strEntry, operation) {
-  et str = '';
+function encodeOrDecode (strEntry, operation) {
+  let str = '';
   if (operation === 'decode') {
     for (let i = 0; i < strEntry.length; i += 1) {
       switch (strEntry[i]) {
@@ -264,3 +264,84 @@ module.exports = {
   splitSentence,
   triangleCheck,
 }
+
+//Tests
+function checkCompareTrue() {
+  (compareTrue (true, true) === true && compareTrue (true, false) === false && compareTrue (false, false) === false) ? console.log('Function compareTrue validated!') : console.log('Function compareTrue invalidated!'); 
+}
+
+function checkCalcArea() {
+  (calcArea(10,1.2) === 6) ? console.log('Function calcArea validated!') : console.log('Function calcArea invalidated!') ;
+}
+
+function checkSplitSentence() {
+  let res = splitSentence('  I study at Trybe every week   ');
+  (res[0] == 'I' && res[1] == 'study' && res[2] == 'at' && res[3] == 'Trybe' && res[4] == 'every' && res[5] == 'week') ? console.log('Function splitSentence validated!') : console.log('Function splitSentence invalidated!') ;
+}
+
+function checkConcatName() {
+(concatName(['I','study','at','Trybe','every','week']) == 'week, I') ? console.log('Function concatName validated!') : console.log('Function concatName invalidated!') ;
+}
+
+function checkFootballPoints() {
+  (footballPoints(30,10) === 100) ? console.log('Function footballPoints validated!') : console.log('Function footballPoints invalidated!') ;
+}
+
+function checkHighestCount() {
+  (highestCount([9, 1, 2, 3, 9, 5, 7]) == 2) ? console.log('Function highestCount validated!') : console.log('Function highestCount invalidated!') ;
+}
+
+function checkCatAndMouse() {
+  (catAndMouse(0, 1, 2) == 'cat1' && catAndMouse(5, 0, 2) == 'cat2' && catAndMouse(1, 0, 2) == 'os gatos trombam e o rato foge') ? console.log('Function catAndMouse validated!') : console.log('Function catAndMouse invalidated!') ;
+}
+
+function checkFizzBuzz() {
+  let res = fizzBuzz([2, 15, 7, 9, 45]);
+  (res[0] == 'bug!' && res[1] == 'fizzBuzz' && res[2] == 'bug!' && res[3] == 'fizz' && res[4] == 'fizzBuzz') ?  console.log('Function fizzBuzz validated!') : console.log('Function fizzBuzz invalidated!') ;
+}
+
+function checkEncode() {
+  (encode('hi there!') === 'h3 th2r2!') ? console.log('Function encode validated!') : console.log('Function encode invalidated!') ;
+}
+
+function checkDecode() {
+  (decode('h3 th2r2!') === 'hi there!') ? console.log('Function decode validated!') : console.log('Function decode invalidated!') ;
+}
+
+function checkTechList() {
+  let res = techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'],'Lucas');
+  (res[0].tech == 'CSS' && res[0].name == 'Lucas' && res[1].tech == 'HTML' && res[1].name == 'Lucas' && res[2].tech == 'JavaScript' && res[2].name == 'Lucas' && res[3].tech == 'Jest' && res[3].name == 'Lucas' && res[4].tech == 'React' && res[4].name == 'Lucas')? console.log('Function techList validated!') : console.log('Function techList invalidated!') ;
+}
+
+function checkGeneratePhoneNumber() {
+  (generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]) == '(12) 34567-8901') ? console.log('Function generatePhoneNumber validated!') : console.log('Function generatePhoneNumber invalidated!') ;
+}
+
+function checkTriangleCheck() {
+  (triangleCheck(10, 14, 8) === true && triangleCheck(5,3,2) === false) ? console.log('Function triangleCheck validated!') : console.log('Function triangleCheck invalidated!') ;
+}
+
+function checkHydrate() {
+  (hydrate('0 cachaça, 5 cervejas e 4 copo de vinho') === '9 copos de água' && hydrate('1 cachaça, 0 cervejas e 0 copo de vinho') === '1 copo de água') ? console.log('Function hydrate validated!') : console.log('Function hydrate invalidated!') ;
+}
+
+//Call to tests
+function testAll() {
+  checkCompareTrue();
+  checkCalcArea();
+  checkSplitSentence();
+  checkConcatName();
+  checkFootballPoints();
+  checkHighestCount();
+  checkCatAndMouse();
+  checkFizzBuzz();
+  checkEncode();
+  checkDecode();
+  checkTechList();
+  checkGeneratePhoneNumber();
+  checkTriangleCheck();
+  checkHydrate();
+
+}
+
+testAll();
