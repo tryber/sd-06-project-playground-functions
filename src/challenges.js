decd3bdb9d51d8528a25b8b72d7703ebb7168e55
STEFANO-BRANZ <stefano.branz@gmail.com>
2020-07-19 21:42:08 -0300

    Requirement bonus 12 second push

--- src/challenges.js
+++ src/challenges.js
@@ -192,6 +192,15 @@ function triangleCheck(lineA, lineB, lineC) {
   if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC)) {
     return true;
   }
+  else if (lineB < (lineA + lineC) && lineB > Math.abs(lineA - lineC)) {
+    return true;
+  }
+  else if (lineC < (lineB + lineA) && lineC > Math.abs(lineB - lineA)) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 // Desafio 13
