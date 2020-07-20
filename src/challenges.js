f5ef7405f52126f839100341906c68dba03f4cb4
us19861229c <rcks.santos@gmail.com>
2020-07-20 09:23:50 -0300

    Corrige alguns erros apontados pelo codeclimate, referentes a: espaçamentos, definição de variáveis, ponto-e-vírgula.

--- src/challenges.js
+++ src/challenges.js
@@ -1,25 +1,25 @@
 // Desafio 1
 function compareTrue(param1, param2) {
-  if(param1 === true && param2 === true){
-    return true
+  if (param1 === true && param2 === true) {
+    return true;
   }
-  return false
+  return false;
 }
 
 // Desafio 2
 function calcArea(base, altura) {
-  return (base * altura)/2
+  return (base * altura) / 2;
 }
 
 // Desafio 3
 function splitSentence(frase) {
-  return frase.split(" ") //necessário refazer!!
+  return frase.split(' '); // necessário refazer!!
 }
 
 // Desafio 4
 function concatName(itens) {
-  let string = itens[itens.length - 1] + ", " + itens[0]
-  return string
+  let string = itens[itens.length - 1] + ', ' + itens[0];
+  return string;
 }
 
 // Desafio 5
@@ -27,151 +27,152 @@ function footballPoints(wins, ties) {
   let valorVitorias = wins * 3;
   let valorEmpates = ties;
   let pontos = valorVitorias + valorEmpates;
-  return pontos
+  return pontos;
 }
 
 // Desafio 6
 function highestCount(valores) {
   let maiorNumero = 0;
   let quantasVezesMaior = 0;
-  for(let indice = 0; indice < valores.length; indice += 1){
-    if(valores[indice] > maiorNumero){
-      maiorNumero = valores[indice]
+  for (let indice = 0; indice < valores.length; indice += 1) {
+    if (valores[indice] > maiorNumero) {
+      maiorNumero = valores[indice];
     }
   }
-  for(let posicao = 0; posicao < valores.length; posicao +=1){
-    if(valores[posicao] === maiorNumero){
-       quantasVezesMaior += 1
+  for (let posicao = 0; posicao < valores.length; posicao += 1) {
+    if (valores[posicao] === maiorNumero) {
+      quantasVezesMaior += 1;
     }
   }
-  return quantasVezesMaior
+  return quantasVezesMaior;
 }
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let posicaoCat1 = 0;
   let posicaoCat2 = 0;
-  let pegaOuNaoPega = ""
-// os valores (mouse, cat1, cat2) não necessariamente incorrem em uma subtração de resultado positivo. Há que considerar a unidade da posição.
-  if(mouse > cat1){
-    posicaoCat1 = mouse - cat1
-  } else if(mouse < cat1){
-    posicaoCat1 = cat1 - mouse
+  let pegaOuNaoPega = '';
+// os valores (mouse, cat1, cat2) não necessariamente incorrem em uma subtração
+// de resultado positivo. Há que considerar a unidade da posição.
+  if (mouse > cat1) {
+    posicaoCat1 = mouse - cat1;
+  } else {
+    posicaoCat1 = cat1 - mouse;
   } // valor da posição do gato1
-  if(mouse > cat2){
-    posicaoCat2 = mouse - cat2
-  } else if( mouse < cat2){
-    posicaoCat2 = cat2 - mouse
+  if (mouse > cat2) {
+    posicaoCat2 = mouse - cat2;
+  } else {
+    posicaoCat2 = cat2 - mouse;
   }// valor da posição do gato2
 
-  if(posicaoCat2 > posicaoCat1){
-    pegaOuNaoPega = "cat1"
-  } else if ( posicaoCat1 > posicaoCat2){
-    pegaOuNaoPega = "cat2"
+  if (posicaoCat2 > posicaoCat1) {
+    pegaOuNaoPega = 'cat1';
+  } else if (posicaoCat1 > posicaoCat2) {
+    pegaOuNaoPega = 'cat2';
   } else {
-    pegaOuNaoPega = "os gatos trombam e o rato foge"
+    pegaOuNaoPega = 'os gatos trombam e o rato foge';
   }// quem poderia pegar o rato ou se nenhum pega
-  return pegaOuNaoPega
-
+  return pegaOuNaoPega;
 }
 
 // Desafio 8
 function fizzBuzz(valoresFizzBuzz) {
-  let novoFizzBuzz = []
-  for(let indice = 0; indice < valoresFizzBuzz.length; indice += 1){
-    if(valoresFizzBuzz[indice] % 3 === 0 && valoresFizzBuzz[indice] % 5 === 0){
-      novoFizzBuzz.push("fizzBuzz")
-    } else if(valoresFizzBuzz[indice] % 3 === 0){
-      novoFizzBuzz.push("fizz")
-    } else if(valoresFizzBuzz[indice] % 5 === 0){
-      novoFizzBuzz.push("buzz")
+  let novoFizzBuzz = [];
+  for (let indice = 0; indice < valoresFizzBuzz.length; indice += 1) {
+    if (valoresFizzBuzz[indice] % 3 === 0 && valoresFizzBuzz[indice] % 5 === 0) {
+      novoFizzBuzz.push('fizzBuzz');
+    } else if (valoresFizzBuzz[indice] % 3 === 0) {
+      novoFizzBuzz.push('fizz');
+    } else if (valoresFizzBuzz[indice] % 5 === 0) {
+      novoFizzBuzz.push('buzz');
     } else {
-      novoFizzBuzz.push("bug!")
+      novoFizzBuzz.push('bug!');
     }
   }
-  return novoFizzBuzz
+  return novoFizzBuzz;
 }
 
 // Desafio 9
 function encode(fraseParaNumero) {
-  arrayFraseNum = fraseParaNumero.split("")
-  for(let indice = 0; indice < arrayFraseNum.length; indice += 1){
+  let arrayFraseNum = fraseParaNumero.split('');
+  for (let indice = 0; indice < arrayFraseNum.length; indice += 1) {
     switch (arrayFraseNum[indice]) {
-      case "a":
+      case 'a':
         arrayFraseNum[indice] = 1;
         break;
-      case "e":
+      case 'e':
         arrayFraseNum[indice] = 2;
         break;
-      case "i":
+      case 'i':
         arrayFraseNum[indice] = 3;
         break;
-      case "o":
+      case 'o':
         arrayFraseNum[indice] = 4;
         break;
-      case "u":
+      case 'u':
         arrayFraseNum[indice] = 5;
+        break;
+      default:
         break;
     }
   }
-  let stringNumeros = arrayFraseNum.join('')
-  return stringNumeros
+  let stringNumeros = arrayFraseNum.join('');
+  return stringNumeros;
 }
 
 function decode(numeroParaFrase) {
-  arrayNumFrase = numeroParaFrase.split("")
-  for(let indice = 0; indice < arrayNumFrase.length; indice += 1){
+  let arrayNumFrase = numeroParaFrase.split('');
+  for (let indice = 0; indice < arrayNumFrase.length; indice += 1) {
     switch (arrayNumFrase[indice]) {
-      case "1":
-        arrayNumFrase[indice] = "a";
+      case '1':
+        arrayNumFrase[indice] = 'a';
         break;
-      case "2":
-        arrayNumFrase[indice] = "e";
+      case '2':
+        arrayNumFrase[indice] = 'e';
         break;
-      case "3":
-        arrayNumFrase[indice] = "i";
+      case '3':
+        arrayNumFrase[indice] = 'i';
         break;
-      case "4":
-        arrayNumFrase[indice] = "o";
+      case '4':
+        arrayNumFrase[indice] = 'o';
         break;
-      case "5":
-        arrayNumFrase[indice] = "u";
+      case '5':
+        arrayNumFrase[indice] = 'u';
+        break;
+      default:
         break;
     }
   }
-  let stringFrase = arrayNumFrase.join('')
-  return stringFrase
+  let stringFrase = arrayNumFrase.join('');
+  return stringFrase;
 }
-
 
 // Desafio 10
 function techList(tecnologias, aluno) {
-  if( tecnologias.length == 0) {
-    return "Vazio!"
-  } else {
-
-    let aprenderTecnologias = []
-    //ordenar as tecnologias (alfabeticamente)
-    for (let indice = 0; indice < tecnologias.length; indice += 1) {
-      for (let comparador = 0; comparador < indice; comparador += 1) {
-        if (tecnologias[indice] < tecnologias[comparador]) {
-          let alocaDado = tecnologias[indice];
-
-          tecnologias[indice] = tecnologias[comparador];
-          tecnologias[comparador] = alocaDado;
-        }
-      } console.log(tecnologias)
-    }
-    //cria um objeto para cada tecnologia e seu aluno
-    for( techs in tecnologias) {
-      criarObjeto = {
-        tech: tecnologias[techs],
-        name: aluno,
-      }
-      aprenderTecnologias.push(criarObjeto)
-    }
-    return aprenderTecnologias
+  if (tecnologias.length === 0) {
+    return 'Vazio!';
   }
+  let aprenderTecnologias = [];
+  // ordenar as tecnologias (alfabeticamente)
+  for (let indice = 0; indice < tecnologias.length; indice += 1) {
+    for (let comparador = 0; comparador < indice; comparador += 1) {
+      if (tecnologias[indice] < tecnologias[comparador]) {
+        let alocaDado = tecnologias[indice];
+
+        tecnologias[indice] = tecnologias[comparador];
+        tecnologias[comparador] = alocaDado;
+      }
+    } console.log(tecnologias);
+  }
+  // cria um objeto para cada tecnologia e seu aluno
+  for (let techs in tecnologias) {
+    let criarObjeto = {
+      tech: tecnologias[techs],
+      name: aluno,
+    };
+    aprenderTecnologias.push(criarObjeto);
+  }
+  return aprenderTecnologias;
 }
 
 // Desafio 11
@@ -205,4 +206,4 @@ module.exports = {
   hydrate,
   splitSentence,
   triangleCheck,
-}
+};
