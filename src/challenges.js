0b046c20a986535f13a7159155f18cb546804d32
FPhilipi <fabiphilipi@gmail.com>
2020-07-20 09:03:34 -0300

    Requisito 10 - tentativa 4, Requisito 11 - tentativa 2 e Requisito 12

--- src/challenges.js
+++ src/challenges.js
@@ -176,9 +176,14 @@ function generatePhoneNumber(array) {
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if ((lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)) && (lineB < lineA + lineC && lineB > Math.abs(lineA - lineC)) && (lineC < lineA + lineB) && lineB > Math.abs(lineA - lineC)) {
+    return true;
+  } else {
+    return false;
+  }
 }
+console.log(triangleCheck(10, 14, 100));
 
 // Desafio 13
 function hydrate() {
