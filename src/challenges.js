6be386730f325cba5a63e278f4bf97f0508f9b2e
Debora Polesel <deborahpolesel@hotmail.com>
2020-07-20 19:30:09 -0300

    Review exercise 8

--- src/challenges.js
+++ src/challenges.js
@@ -69,11 +69,11 @@ function catAndMouse(mouse, cat1, cat2) {
 function fizzBuzz(arrayNumbers) {
   let arrayFizzBuzz = [];
   for (let i in arrayNumbers) {
-    if ((arrayNumbers[i] % 3)===0 && (arrayNumbers[i] % 5)===0) {
+    if (arrayNumbers[i] % 3===0 && arrayNumbers[i] % 5===0) {
         arrayFizzBuzz.push('fizzBuzz!');
-    } else if ((arrayNumbers[i] % 5)===0 && (arrayNumbers[i] % 3)!==0) {
+    } else if (arrayNumbers[i] % 5===0 && arrayNumbers[i] % 3!==0) {
         arrayFizzBuzz.push('buzz!');
-    } else if ((arrayNumbers[i] % 3)===0 && (arrayNumbers[i] % 5)!==0) {
+    } else if (arrayNumbers[i] % 3===0 && arrayNumbers[i] % 5!==0) {
         arrayFizzBuzz.push('fizz!');
     } else {
         arrayFizzBuzz.push('bug!');
