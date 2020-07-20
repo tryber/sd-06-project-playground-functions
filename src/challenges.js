7be39b5aa771f2926e74d403b2e504f2a259b927
paulo-lins <paulolins100@gmail.com>
2020-07-20 20:39:21 -0300

    desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -59,9 +59,15 @@ highestCount(array)
 
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if ((mouse - cat1) < (mouse - cat2)) {
+    return cat1} 
+    else if ((mouse - cat1) > (mouse - cat2)) {
+    return cat2} else {
+      return "os gatos trombam e o rato foge"
+    }
 }
+
 
 // Desafio 8
 function fizzBuzz() {
