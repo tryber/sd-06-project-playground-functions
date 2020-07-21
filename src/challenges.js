f1045815e008a41e5bd7cf3c5841d6cf6702c2ac
CleberFontinele <cleberfontinele2387@gmail.com>
2020-07-21 12:01:29 -0300

    Repetição do maior número

--- src/challenges.js
+++ src/challenges.js
@@ -29,7 +29,7 @@ function concatName(string) {
   // seu código aqui
   let palavras = '';
   palavras += string[string.length - 1];
-  palavras += ', '
+  palavras += ', ';
   palavras += string[0];
   return palavras;
 }
@@ -38,14 +38,27 @@ function concatName(string) {
 function footballPoints(wins, ties) {
   // seu código aqui
   let somaPontos = (wins * 3) + ties;
-  return somaPontos
+  return somaPontos;
 }
 // console.log(footballPoints(1, 3));
 
 // Desafio 6
-function highestCount() {
+function highestCount(array) {
   // seu código aqui
-}
+  let qtdDeVezes = 0;
+  let maiorNumero = array[0];
+  for (let index in array) {
+      if (maiorNumero < array[index]){
+    }
+  }
+    for (let index in array){
+       if (array[index] === maiorNumero){
+         qtdDeVezes += 1;
+        }
+    }
+    return qtdDeVezes;
+  }
+// console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
 
 // Desafio 7
 function catAndMouse() {
@@ -101,4 +114,4 @@ module.exports = {
   hydrate,
   splitSentence,
   triangleCheck,
-}
+};
