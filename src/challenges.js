7ddf2f6f7c45770e86a54366855e9514b80072b2
FPhilipi <fabiphilipi@gmail.com>
2020-07-21 04:19:32 -0300

    Requisito 11

--- src/challenges.js
+++ src/challenges.js
@@ -172,7 +172,7 @@ if (array.length > 11 || array.length < 11) {
                 }                
             } else {
                 if (index === array.length - 1) {
-                return "(" + array[0] + array[1] + ")" + array[2] + array[3] + array[4] + array[5] + array[6] + "-" + array[7] + array[8] + array[9] + array[10];
+                return "(" + array[0] + array[1] + ") " + array[2] + array[3] + array[4] + array[5] + array[6] + "-" + array[7] + array[8] + array[9] + array[10];
             }
         } 
     }
