b76a833314f4466ebcfbb3f07ad8d2bc98027dc5
Zeca Costa <jccostaso@gmail.com>
2020-07-21 06:36:33 -0300

    Atualizaçao para incluir desafio bonus  #12

--- src/challenges.js
+++ src/challenges.js
@@ -354,10 +354,10 @@ function generatePhoneNumber(phoneNumber) {
           threeTimesCounter += 1;
         }
       }
-        if (threeTimesCounter >= 3 || numbers < 0 ||numbers > 9) {
-          testResult2 = true
+        if (threeTimesCounter >= 3 || numbers < 0 || numbers > 9) {
+          testResult2 = true;
         }
-    threeTimesCounter = 0;
+      threeTimesCounter = 0;
     }
   }
   if (testResult1 === true) {
@@ -370,13 +370,41 @@ function generatePhoneNumber(phoneNumber) {
   return returnMessage;
 }
 
-// let testArray = [1, 5, 9, 9, 9, 7, 4, 6, 4, 5, 8];
+// let testArray = [1, 5, 9, 9, 7, 7, 4, 6, 4, 5, 8];
 // console.log(generatePhoneNumber(testArray));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+
+// Condição de existência de um triângulo
+//Um triângulo é composto de três linhas: lineA, lineB e lineC. Crie uma função chamada
+// triangleCheck que deverá receber as três linhas como parâmetro e retornar se
+// é possível formar um triângulo com os valores apresentados de cada linha
+
+// Para tanto, tenha em mente algumas considerações:
+
+// Para que seja possível formar um triângulo, é necessário que
+// a medida de qualquer um dos lados seja menor que a soma das medidas
+// dos outros dois e maior que o valor absoluto da diferença entre essas medidas.
+
+// Para obter o valor absoluto de um número em JavaScript, pesquise pela função Math.abs.
+
+// O retorno da sua função deverá ser um booleano.
+
+// Exemplo: o retorno de triangleCheck(10, 14, 8) deverá ser true.
+
+function triangleCheck(lineA, lineB, lineC) {
+  let returnMessage = false;
+
+  if (lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB) {
+    returnMessage = true;
+  }
+  if (lineA > Math.abs(lineB - lineC) && lineB > Math.abs(lineA - lineC) && lineC > Math.abs(lineA - lineB)) {
+    returnMessage = true;
+  }
+  return returnMessage;
 }
+
+// console.log(triangleCheck(10, 18, 8));
 
 // Desafio 13
 function hydrate() {
