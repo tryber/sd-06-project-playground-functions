4034f9d02809a90581212e7d410a962f8034b233
gussta <saideles.silveira@gmail.com>
2020-07-21 08:42:13 -0300

    desafio 6.. agora ta apertando

--- src/challenges.js
+++ src/challenges.js
@@ -23,20 +23,35 @@ function splitSentence(frase) {
 function concatName(lista) {
   let extremos;
   extremos = `${lista[lista.length - 1]}, ${lista[0]}`;
-  return extremos
-  //esse $ foi ideia do VScode, mas parece fazer sentido... não topei a ideia de colocar em outro arquivo .js
+  return extremos;
+  // esse $ foi ideia do VScode, mas parece fazer sentido
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let pontos
-  pontos = (wins * 3) + ties
-  return pontos
+  let pontos;
+  pontos = (wins * 3) + ties;
+  return pontos;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numerosLista) {
+  //Escreva uma função chamada `highestCount` que, ao receber uma array de números, retorne  a quantidade de vezes que o maior deles se repete.
+
+  //Exemplo: caso o parâmetro de `highestCount` seja uma array com valores `[9, 1, 2, 3, 9, 5, 7]`, a função deverá retornar `2`, que é a quantidade de vezes que o número `9` (maior número do array) se repete.
+  let numerosQuantid = 0;
+  let i;
+  let maiorNumero = 0;
+  for (i = 0; i < numerosLista.length; i += 1) {
+    if (numerosLista[i] > maiorNumero) {
+      numerosLista[i] = maiorNumero;
+      numerosQuantid = 1;
+    } else if (numerosLista[i] === maiorNumero) {
+      numerosQuantid + 1;
+    }
+  }
+  return numerosQuantid;
+
 }
 
 // Desafio 7
