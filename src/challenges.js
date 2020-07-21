13d10bfb2538f81c348e181b73884fda075fde83
Debora Polesel <deborahpolesel@hotmail.com>
2020-07-21 14:31:36 -0300

    "Exercise 11"

--- src/challenges.js
+++ src/challenges.js
@@ -157,14 +157,12 @@ function generatePhoneNumber(arrayNumber) {
   let listNumber = arrayNumber.sort();
   let numbersRepeated = 0;
   for (let index4=0; index4<listNumber.length;index4++) {
-    if (listNumber[index4] === listNumber[index4 -1]) {
-      numbersRepeated +=1;
+    if (listNumber[index4] === listNumber[index4 -1] === listNumber[index4 -2]) {
+      return "não é possível gerar um número de telefone com esses valores";
     }
   }
   if (arrayNumber.length !== 11) {
     return "Array com tamanho incorreto.";
-  } else if (numbersRepeated > 2) {
-    return "não é possível gerar um número de telefone com esses valores";
   } else {
     for (let index3 =0; index3 <arrayNumber.length; index3++) {
       if (arrayNumber[index3]<0 || arrayNumber[index3]>9) {
