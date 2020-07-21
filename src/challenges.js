aa0f517348473dca8485df7f94f1e3d1ba81d761
FPhilipi <fabiphilipi@gmail.com>
2020-07-21 03:59:25 -0300

    Requisito 11

--- src/challenges.js
+++ src/challenges.js
@@ -153,26 +153,32 @@ return arrayThree;
 console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Fabi"));
 
 // Desafio 11
-function generatePhoneNumber(array) {
-  if (array.length > 11 || array.length < 11) {
-      return "Array com tamanho incorreto.";
-  } else {
-      for (let index in array) {
-          let contador = 0;
-          if (array[index] < 0 || array[index] > 9) {
-              return "não é possível gerar um número de telefone com esses valores";
-          } else if (array[index] === array [index + 1]) {
-              contador += 1;
-              if (contador >= 3) {
-                  return "não é possível gerar um número de telefone com esses valores";
-              }
-          } else {
-              let string = "(" + array[0] + array[1] + ")" + array[2] + array[3] + array[4] + array[5] + array[6] + "-" + array[7] + array[8] + array[9] + array[10];
-              return string;                
-          }      
-      }
-  }
+function generatePhoneNumber(array) { 
+
+let contador = 0;
+
+if (array.length > 11 || array.length < 11) {
+    return "Array com tamanho incorreto.";
+    } else {
+        for (let index = 0; index < array.length; index += 1) {
+            if (array[index] < 0 || array[index] > 9) {
+            return "não é possível gerar um número de telefone com esses valores";
+            }
+            else if (array[index] === array[index + 1]) {
+                contador = contador + 1;
+                if (contador > 1) {
+                return "não é possível gerar um número de telefone com esses valores";
+                break;
+                }                
+            } else {
+                if (index === array.length - 1) {
+                return "(" + array[0] + array[1] + ")" + array[2] + array[3] + array[4] + array[5] + array[6] + "-" + array[7] + array[8] + array[9] + array[10];
+            }
+        } 
+    }
+    }
 }
+
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
