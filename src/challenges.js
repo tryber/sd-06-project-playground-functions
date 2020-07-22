4a162e588962d9b5ba51cd515728e9aac9246616
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-07-21 22:27:59 -0300

    11° exercicio reajuste agora rejeita numeros que aparecem mais de 3 vezes

--- src/challenges.js
+++ src/challenges.js
@@ -197,23 +197,21 @@ console.log(techList(valor1, valor2)); */
 // Desafio 11
 function generatePhoneNumber(arrayTelefone) {
   // seu código aqui
-  for (let phoneN1 in arrayTelefone) {
-    let maisDe3Vezes = 0;
-    for (let phoneN2 in arrayTelefone) {
-      if (phoneN1 === phoneN2) {
-        maisDe3Vezes += 1;
-      }
-      if (maisDe3Vezes >= 3){
-        break;
-      }
-    }
-  }
   if (arrayTelefone.length !== 11){
     return "Array com tamanho incorreto."    
   }else{
     let escrevendoNumero = "";     
     for (let numero in arrayTelefone){
-      if (arrayTelefone[numero] < 0 || arrayTelefone[numero] > 9 || maisDe3Vezes <= 3){        
+      let maisDe3Vezes = 0;
+      for (let phoneN in arrayTelefone) {
+        if (arrayTelefone[numero] === arrayTelefone[phoneN]) {
+          maisDe3Vezes += 1;
+        }
+        if (maisDe3Vezes >= 3){
+          break;
+        }
+      }
+      if (arrayTelefone[numero] < 0 || arrayTelefone[numero] > 9 || maisDe3Vezes >= 3){        
         return "não é possível gerar um número de telefone com esses valores"      
         break;
       }else if (numero === "0"){
@@ -230,7 +228,6 @@ function generatePhoneNumber(arrayTelefone) {
   }
 }
 /* let valor1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
-let valor2 = "Clayton Miguel";
 console.log(generatePhoneNumber(valor1)); */
 
 // Desafio 12
