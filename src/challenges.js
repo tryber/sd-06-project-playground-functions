9e04183ad41eff0845f11cc432c47422eeb31070
Aurelio Scarante <aurelioscarante25@gmail.com>
2020-07-22 07:14:10 -0300

    Arrumando possíveis erros nos exercícios 1, 2, 3, 4, 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,31 +1,36 @@
 // Desafio 1
-function compareTrue( valor1 , valor2 ) {
-  let resultado = valor1 && valor2
-  return resultado;
+function compareTrue(value1 , value2) {
+  return value1 && value2
 }
+console.log(compareTrue(true, true))
 
-// Desafio 2
+//Desafio 2
 function calcArea( base, heigth ) {
-  let resultado = (base * heigth ) / 2;
-  return resultado;
+  let area = (base * heigth) / 2
+  return area;
 }
-
+console.log(calcArea( 5, 10 ));
+ 
 // Desafio 3
-function splitSentence( name ) {
-  name = "Go Trybe";
-  let resultado = name.split(" ", 2);
-  return resultado
+function splitSentence( prhase ) {
+  let splitPhrase = prhase.split(" ");
+  return splitPhrase;
 }
+console.log(splitSentence("Aurelio Scarante Taborda"));
 
-// Desafio 4
-function concatName(["Olá mundo"]) {
-  // seu código aqui
+//Desafio 4
+function concatName([listaNomes]) {
+  return listaNomes[listaNomes.lenght - 1] + ", " + listaNomes[0];
 }
+console.log(concatName["Aurelio, Lorenzo, Millena"]);
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let totalPoints = (wins  * 3) + ties;
+  return totalPoints
 }
+
+console.log(footballPoints(5, 5));
 
 // Desafio 6
 function highestCount() {
