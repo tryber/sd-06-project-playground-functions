a3bc36b7cff39292c198c146f81ed612410ee19b
Rods SSD <rods.leite27@gmail.com>
2020-07-22 13:54:30 -0300

    Requisito 13

--- src/challenges.js
+++ src/challenges.js
@@ -163,18 +163,17 @@ function triangleCheck(a,b,c) {
 
 // Desafio 13
 function hydrate(string){
-  let num = string.match(/\d+/g)
-  soma = 0
-  for(i in num){
-    soma = soma + Number(num[i])
+    let num = string.match(/\d+/g)
+    soma = 0
+    for(i in num){
+      soma = soma + Number(num[i])
+    }
+    if(soma <= 1){
+        return soma + " copo de água"
+    }else{
+        return soma + " copos de água"
+    }
   }
-  if(soma <= 1){
-      return soma + " copo de água"
-  }else{
-      return soma + " copos de água"
-  }
-}
-console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'))
 
 module.exports = {
   calcArea,
