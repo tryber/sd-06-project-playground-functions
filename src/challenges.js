1498299d007e0e0adb014b48690c9d5f23b2edad
Anderson <and.stuber@gmail.com>
2020-07-23 01:17:58 -0300

    adjustments exercise 11

--- src/challenges.js
+++ src/challenges.js
@@ -100,15 +100,15 @@ function techList(t = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], name = 'Lu
 
 // Desafio 11
 function generatePhoneNumber(numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 1]) {
-  if (numbers.length != 11) { console.log('Array com tamanho incorreto.') };
+  if (numbers.length != 11) { 
+    return 'Array com tamanho incorreto.'; }
   let firsts = [];
   let lasts = [];
   for (let i = 0; i < numbers.length; i += 1) {
     let number = numbers[i];
     if (number < 0 || number > 9) { 
-      console.log('não é possível gerar um número de telefone com esses valores') 
-      return;
-    };
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
   }
 
   for (let i = 0; i < numbers.length; i += 1) {
@@ -121,7 +121,7 @@ function generatePhoneNumber(numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 1]) {
   }
   let ddd = '' + numbers[0] + numbers[1];
   let add = numbers[2];
-  console.log(`(${ddd}) ${add} ${firsts}-${lasts}`);
+  return `(${ddd}) ${add} ${firsts}-${lasts}`;
 }
 generatePhoneNumber();
 
