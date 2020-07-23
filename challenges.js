53415fd22d911d9d7f0e37e310082847517cbccc
Artur Forna <fornagieri.artur@gmail.com>
2020-07-23 07:40:37 -0300

    Exercicio 01 - Finalizado

---
+++ challenges.js
@@ -0,0 +1,22 @@
+//01 - Compara valores booleanos
+compareTrue(true,true)
+
+function compareTrue(boolean1,boolean2){
+  let firstValue = boolean1;
+  let secondValue = boolean2;
+
+  if(firstValue == true && secondValue == true){
+    return console.log('true');
+  }else{
+    return console.log('false');
+  }
+}
+
+//02 - Calcula área do triangulo
+
+calcArea();
+
+function calcArea(){
+  
+}
+
