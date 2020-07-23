c5256ff2464a854d6fae596099683511bf199142
Dhiego Dias <ddias003@gmail.com>
2020-07-23 11:23:21 -0300

    8 primeiros exercicios, vamo que vamo

--- src/challenges.js
+++ src/challenges.js
@@ -12,34 +12,75 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ');
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let solucao;
+  let primeiro = array[0];
+  let ultimo = array[array.length - 1];
+  solucao = (ultimo + ', ' + primeiro);
+  return solucao;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numeral) {
+  let maior = numeral[0];
+  let inicio = 0;
+
+  for (let i = 0; i < numeral.length; i += 1) {
+    if (maior === inicio[i]) {
+      inicio += 1;
+    }
+
+    if (numeral[i] > maior) {
+      maior = numeral[i];
+
+      inicio = 1;
+    }
+    
+  }
+  return inicio;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let Gato1 = Math.abs(cat1 - mouse);
+  let Gato2 = Math.abs(cat2 - mouse);
+
+  if (Gato1 === Gato2) {
+    return 'os gatos trombam e o rato foge';
+  } else if (Gato1 < Gato2) {
+    return 'cat1';
+  }
+  return 'cat2';
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+function fizzBuzz(parametro) {
+  let resultado = [];
+
+  for (let i = 0; i < parametro.length; i += 1) {
+    let parametro3 = parametro[i] % 3 === 0;
+    let parametro5 = parametro[i] % 5 === 0;
+    if (parametro3 && parametro5) {
+      resultado.push('FizzBuzz');
+    } else if (parametro3) {
+      resultado.push('Fizz');
+    } else if (parametro5) {
+      resultado.push('Buzz');
+    } else {
+      resultado.push('Bug!');
+    }
+  }
+} 
 
 // Desafio 9
 function encode() {
