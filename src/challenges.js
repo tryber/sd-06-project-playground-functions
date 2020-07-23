d1a8c70709bdb7a298fd7281c3788840d215240f
Aurelio Scarante <aurelioscarante25@gmail.com>
2020-07-23 07:00:07 -0300

    Desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -5,10 +5,8 @@ function compareTrue(value1 , value2) {
 
 //Desafio 2
 function calcArea( base, heigth ) {
-  let area = (base * heigth) / 2
-  return area;
+  return (base * heigth) / 2
 }
-console.log(calcArea( 5, 10 ));
  
 // Desafio 3
 function splitSentence( prhase ) {
@@ -16,19 +14,32 @@ function splitSentence( prhase ) {
   return splitPhrase;
 }
 //Desafio 4
-function concatName([listaNomes]) {
-  return listaNomes[listaNomes.lenght - 1] + ", " + listaNomes[0];
+function concatName(names) {
+  let listNames = [names.lenght - 1] + ", " + names[0];
+  return listNames
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let totalPoints = (wins  * 3) + ties;
+  let totalPoints = (wins * 3) + ties;
   return totalPoints
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers) {
+  let numMaior = 0;
+  let total = 0;
+  for (index = 0; index <= numbers.lenght; index += 1){
+    if (numbers[index] > numMaior){
+      numMaior = numbers[index]
+    } 
+  }
+  for (index2 = 0; index2 <= numbers.lenght; index2 += 1){
+    if (numbers[index2] === numMaior) {
+      total += 1;// seu código aqui
+    }
+  }
+  return total
 }
 
 // Desafio 7
