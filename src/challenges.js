8f4cac8dffdaa2f9fa12506072143bb2c4678782
Guilherme Persch <persch.gui@gmail.com>
2020-07-24 14:55:51 -0300

    Corrigind espaços e sintaxe

--- src/challenges.js
+++ src/challenges.js
@@ -1,21 +1,21 @@
 // Desafio 1
-function compareTrue(parametro1, parametro2){
+function compareTrue(parametro1, parametro2) {
   if (parametro1 && parametro2) {
     return true;
-  }else {
+  } else {
     return false;
   }
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base*height)/2;
+  let area = (base * height)/2;
   return area;
 }
 
 // Desafio 3
 function splitSentence(parametroString) {
-  array = parametroString.split(" ");
+  let array = parametroString.split(" ");
   return array;
 }
 
@@ -25,7 +25,7 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints(wins, ties){
+function footballPoints(wins, ties) {
   pontos = (wins*3)+(ties);
   return pontos;
 }
@@ -35,12 +35,12 @@ function highestCount(array) {
   let identificaMaior = 0;
   let numeroRepetido = 0;
   for (let i in array){
-    if (array[i]>identificaMaior){
+    if (array[i]>identificaMaior) {
       identificaMaior = array[i];
     }
   }
-  for (let i in array){
-    if (array[i]==identificaMaior){
+  for (let i in array) {
+    if (array[i]==identificaMaior) {
       numeroRepetido += 1;
     }
   }
@@ -51,9 +51,9 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
   let distanceCat1 = mouse - cat1;
   let distanceCat2 = mouse - cat2;
-  if(distanceCat1 < distanceCat2){
+  if(distanceCat1 < distanceCat2) {
     return cat1;
-  }else if(distanceCat2 < distanceCat1){
+  } else if(distanceCat2 < distanceCat1) {
     return cat2;
   }else{
     return resposta = "os gatos trombam e o rato foge";
@@ -62,14 +62,14 @@ function catAndMouse(mouse, cat1, cat2) {
 
 // Desafio 8
 function fizzBuzz(array) {
-  for(let i in array){
-    if(((array[i]%3) == 0)&&((array[i]%5) == 0)){
+  for(let i in array) {
+    if(((array[i]%3) == 0)&&((array[i]%5) == 0)) {
       return "fizzBuzz";
-    }else if((array[i]%5) == 0){
+    } else if((array[i]%5) == 0) {
       return "buzz";
-    }else if((array[i]%3) == 0){
+    } else if((array[i]%3) == 0) {
       return "fizz";
-    }else{
+    } else {
       return "bug!";
     }
   }
@@ -77,16 +77,16 @@ function fizzBuzz(array) {
 
 // Desafio 9
 function encode(parametro4) {
-  for(let i in parametro4){
-    if(parametro4[i]== "a"){
+  for(let i in parametro4) {
+    if(parametro4[i]== "a") {
       parametro4[i].push = 1;
-    }else if(parametro4[i]== "e"){
+    } else if(parametro4[i]== "e") {
       parametro4[i].push = 2;
-    }else if(parametro4[i]== "i"){
+    } else if(parametro4[i]== "i") {
       parametro4[i].push = 3;
-    }else if(parametro4[i]== "o"){
+    } else if(parametro4[i]== "o") {
       parametro4[i].push = 4;
-    }else if(parametro4[i]== "u"){
+    } else if(parametro4[i]== "u") {
       parametro4[i].push = 5;
     }
   }
