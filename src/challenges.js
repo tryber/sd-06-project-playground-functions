49a1ac3eb0c1f039b4765ab0b5b7cdf2142b0af5
Marcelo Pessini <marceloppessini@gmail.com>
2020-07-24 11:01:58 -0300

    Testando função telefone

--- src/challenges.js
+++ src/challenges.js
@@ -167,7 +167,7 @@ function generatePhoneNumber(array) {
     return "não é possível gerar um número de telefone com esses valores";
   } else {
     return "(" + array[0] + array[1] + ") " + array[2] + array[3] + array[4] + array[5] + array[6] + "-" + array[7] + array[8] + array[9] + array[10];
-  }
+    }
 }
 
 
