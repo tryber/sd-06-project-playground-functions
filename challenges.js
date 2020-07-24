b95e836919888f93e2bde6531dd9050ebbbca08b
Artur Forna <fornagieri.artur@gmail.com>
2020-07-24 20:59:55 -0300

    Tentando denovo e denovo

--- challenges.js
+++ challenges.js
@@ -138,22 +138,18 @@ function decode(string){
 
 // Desafio 10
 function techList() {
-  // seu código aqui
 }
 
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
 }
 
 // Desafio 12
 function triangleCheck() {
-  // seu código aqui
 }
 
 // Desafio 13
 function hydrate() {
-  // seu código aqui
 }
 
 module.exports = {
