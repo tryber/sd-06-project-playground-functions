5df96fa20ee2eaa8d98e577759572da9cdd71f10
Guilherme Persch <persch.gui@gmail.com>
2020-07-24 14:41:52 -0300

    Fixing desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -62,7 +62,7 @@ function catAndMouse(mouse, cat1, cat2) {
 
 // Desafio 8
 function fizzBuzz(array) {
-  for(let i = 0; i < array.leght; i += 1){
+  for(let i in array){
     if(((array[i]%3) == 0)&&((array[i]%5) == 0)){
       return "fizzBuzz";
     }else if((array[i]%5) == 0){
