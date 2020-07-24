9ffe63bc85eeba29a9c00efefb6b880987621dd8
Debora Polesel <deborahpolesel@hotmail.com>
2020-07-24 00:50:11 -0300

    "Exercise 11"

--- src/challenges.js
+++ src/challenges.js
@@ -154,22 +154,28 @@ function techList(technologies,name) {
 // Desafio 11 
 function generatePhoneNumber(arrayNumber) {
   let phoneNumber = 0;
+  let repeatedNumber = 0;
+  let numberWrong = 0;
   let listNumber = arrayNumber.sort();
-  for (let index4=0; index4<listNumber.length;index4++) {
-    if (listNumber[index4] === listNumber[index4 -1] && listNumber[index4] === listNumber[index4 -2]) {
-      return "não é possível gerar um número de telefone com esses valores";
-    }
+  for (let i=0; i<listNumber.length;i++) {
+      if (listNumber[i] === listNumber[i+1] && listNumber[i] === listNumber[i+2]) {
+        repeatedNumber += 1;
+      }
+  } 
+  for (let i=0; i <arrayNumber.length; i++) {
+      if (arrayNumber[i]<0 || arrayNumber[i]>9) {
+        numberWrong += 1;
+      }
   }
   if (arrayNumber.length !== 11) {
-    return "Array com tamanho incorreto.";
+      return "Array com tamanho incorreto.";
+  } else if (repeatedNumber !==0) {
+      return "não é possível gerar um número de telefone com esses valores";
+  } else if (numberWrong!==0) {
+      return "não é possível gerar um número de telefone com esses valores";
   } else {
-    for (let index3 =0; index3 <arrayNumber.length; index3++) {
-      if (arrayNumber[index3]<0 || arrayNumber[index3]>9) {
-        return "não é possível gerar um número de telefone com esses valores";
-      } phoneNumber = `(${arrayNumber[0]}${arrayNumber[1]}) ${arrayNumber[2]}${arrayNumber[3]}${arrayNumber[4]}${arrayNumber[5]}${arrayNumber[6]}-${arrayNumber[7]}${arrayNumber[8]}${arrayNumber[9]}${arrayNumber[10]}`;
-    }
-  }
-  return phoneNumber;
+      phoneNumber = `(${arrayNumber[0]}${arrayNumber[1]}) ${arrayNumber[2]}${arrayNumber[3]}${arrayNumber[4]}${arrayNumber[5]}${arrayNumber[6]}-${arrayNumber[7]}${arrayNumber[8]}${arrayNumber[9]}${arrayNumber[10]}`;
+  } return phoneNumber;
 }
 
 // Desafio 12
