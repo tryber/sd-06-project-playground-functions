db875de6bdae07804b7fbd6fb0d732b87f783795
v-icto-r <victor130496@gmail.com>
2020-07-25 12:14:44 -0300

    alguns acrescimos

--- src/challenges.js
+++ src/challenges.js
@@ -1,41 +1,99 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(a, b) {
   // seu código aqui
+  let resultado;
+  if (a === true && b === true) {
+    resultado = true;
+  } else {
+    resultado = false;
+  }
+  return resultado;
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, altura) {
   // seu código aqui
+  return (base * altura) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(nome) {
   // seu código aqui
+  let resultado = nome.split(' ');
+  return resultado;
 }
 
 // Desafio 4
-function concatName() {
+function concatName(arr) {
   // seu código aqui
+  let primeiro = arr[0];
+  let ultimo = arr[arr.length - 1];
+  let str = ultimo + ', ' + primeiro;
+  return str;
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(vitoria, empate) {
   // seu código aqui
+  vitoria *= 3;
+  empate *= 1;
+  return vitoria + empate;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(inteiros) {
   // seu código aqui
+  let indiceMaior = 0;
+  let contador = 0;
+
+  for (let indice in inteiros) {
+      if(inteiros[indiceMaior] < inteiros[indice]){
+          indiceMaior = indice;
+      }
+  }
+  for (let i in inteiros) {
+      if (inteiros[indiceMaior] === inteiros[i]) {
+          contador += 1;
+      }
+  }
+  return contador;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaC1 = Math.abs(mouse - cat1);
+  let distanciaC2 = Math.abs(mouse - cat2);
+  let msg;
+  
+  if (mouse) {
+
+  }
+  if (distanciaC1 > distanciaC2) {
+    msg = "cat2";
+  } else if (distanciaC2 > distanciaC1) {
+    msg = "cat1";
+  } else {
+    msg = "os gatos trombam e o rato foge";
+  }
+  return msg;
 }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(arr) {
   // seu código aqui
+  let resultados = [];
+  for ( let i in arr ) {
+    if ( (arr[i] % 3 === 0) && (arr[i] % 5 === 0) ) {
+      resultados.push('fizzBuzz');
+    } else if ( arr[i] % 3 === 0 ) {
+        resultados.push('fizz');
+    } else if ( arr[i] % 5 === 0 ) {
+        resultados.push('buzz');
+    } else {
+      resultados.push('bug!');
+    }
+  }
+  return resultados;
 }
 
 // Desafio 9
@@ -47,8 +105,23 @@ function decode() {
 }
 
 // Desafio 10
-function techList() {
+function techList(arr, nome) {
   // seu código aqui
+  let resultados = [];
+  let msg = 'Vazio!';
+  if ( arr.length > 0) {
+    for ( let i in arr ) {
+      resultados.push( {tech: arr[i], name: nome} );
+    }
+    resultados.sort(function (a, b) {
+	
+    return (a.tech > b.tech) ? 1 : ((b.tech > a.tech) ? -1 : 0);
+     
+    });
+    return resultados;
+  } else {
+    return msg;
+  }
 }
 
 // Desafio 11
