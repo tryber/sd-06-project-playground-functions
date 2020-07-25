7ecc3e1315468605ed6aee5ca499020c324514c7
bragammartins <bragammartins@gmail.com>
2020-07-25 08:55:59 -0300

    4 atividade - 4 - correcting codeclimate errors

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,10 @@
 /* começa mais um desafio */
 
 // Desafio 1
-/* 1 - Usando o operador && JavaScript possui um operador lógico &&, o qual 
-recebe dois valores e retorna true se ambos os valores são verdadeiros, e 
+/* 1 - Usando o operador && JavaScript possui um operador lógico &&, o qual
+recebe dois valores e retorna true se ambos os valores são verdadeiros, e
 retorna false se algum dos valores não o for.
-Considerando isso, crie uma função chamada compareTrue que, ao receber 
+Considerando isso, crie uma função chamada compareTrue que, ao receber
 dois booleanos: Retorne true se ambos os valores são verdadeiros;
 Retorne false se um ou ambos os parâmetros forem falsos.
 Faça a função utilizando o operador &&.
