2da39cd005b22722cc44e894a20fd2135c0d21c9
ilanribeiro <ilanribeiro@gmail.com>
2020-07-25 12:08:45 -0300

    updated to challenge 11 version2

--- src/challenges.js
+++ src/challenges.js
@@ -123,8 +123,36 @@ function techList(techNameArray, named) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  //variables
+  let formattedPhoneNumber = '';
+  let ddd = `(${array[0]}${array[1]})`;
+  let firstPartPhoneNumber = `${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}`;
+  let secondPartPhoneNumber = `${array[7]}${array[8]}${array[9]}${array[10]}`;
+
+  // compare repeated numbers within array
+  for (let index = 0; index < array.length; index += 1) {
+    // variable to count the number of repetitions
+    let countRepeat = 0;
+
+    for (let indexTwo = 0; indexTwo < array.length; indexTwo += 1) {
+      if (array[index] === array[indexTwo]) {
+        countRepeat += 1
+      }
+    }
+    // Verifying impossible conditions to generate a phone number
+    if (countRepeat >= 3 || array[index] < 0 || array[index] > 9) {
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+  }
+  // verifying permitted length conditions to generate a phone number
+  if (array.length !== 11) {
+    formattedPhoneNumber = "Array com tamanho incorreto."
+  } else {
+    formattedPhoneNumber = `${ddd} ${firstPartPhoneNumber}-${secondPartPhoneNumber}`;
+  }
+
+  return formattedPhoneNumber;
 }
 
 // Desafio 12
