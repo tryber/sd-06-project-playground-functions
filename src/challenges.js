1065a464e82414263a2709fe0f92d9c72cbaae19
Aurelio Scarante <aurelioscarante25@gmail.com>
2020-07-25 11:21:15 -0300

    Inserindo 'Math.abs' no exercício 9

--- src/challenges.js
+++ src/challenges.js
@@ -49,8 +49,8 @@ function highestCount(numbersList) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let distanciaCat1 = cat1 - mouse;
-  let distanciaCat2 = cat2 - mouse;
+  let distanciaCat1 = Math.abs(cat1 - mouse);
+  let distanciaCat2 = Math.abs(cat2 - mouse);
   if (distanciaCat1 === distanciaCat2) {
     return "os gatos trombam e o rato foge"
   } else if (distanciaCat2 < distanciaCat1) {
